14e743d116e9c0ce8a91d4c9d1357ea4bfd4682c
drsung <drsung@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2003-11-27 08:30:54 +0000

    Bug fix
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@145 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- pro/dencode.c
+++ pro/dencode.c
@@ -1,3 +1,42 @@
+/*
+ * Copyright (C) 2001-2003 by egnite Software GmbH. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY EGNITE SOFTWARE GMBH AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL EGNITE
+ * SOFTWARE GMBH OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * For additional information see http://www.ethernut.de/
+ */
+
+/*
+ * $Log$
+ * Revision 1.2  2003/11/27 08:30:54  drsung
+ * Bug fix
+ *
+ */
+ 
 #include "dencode.h"
 
 /* Base-64 decoding.  This represents binary data as printable ASCII
@@ -38,11 +77,12 @@ static prog_char base64dtab[256] = {
  */
 char *NutDecodeBase64(u_char * str)
 {
-    int code;
+    /* bug fix from Damian Slee. */
+    char code;
     char *sp;
     char *tp;
-    int last = -1;
-    int step = 0;
+    char last = -1;
+    char step = 0;
 
     for (tp = sp = str; *sp; ++sp) {
         if ((code = PRG_RDB(&base64dtab[(int) *sp])) == -1)
