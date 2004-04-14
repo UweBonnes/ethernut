a5d3382775768d2567a074cf9cf2dfcd8d369f5e
freckle <freckle@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2004-04-14 18:30:58 +0000

    This is Revision 1.1.1.1 of errno.h. Revision 1.2 was checked in accidently
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@324 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- include/net/errno.h
+++ include/net/errno.h
@@ -1,11 +1,40 @@
+#ifndef _NET_ERRNO_H_
+#define _NET_ERRNO_H_
+
 /*
- * Copyright (c) 1982, 1986, 1989, 1993
- *	The Regents of the University of California.  All rights reserved.
- * (c) UNIX System Laboratories, Inc.
- * All or some portions of this file are derived from material licensed
- * to the University of California by American Telephone and Telegraph
- * Co. or Unix System Laboratories, Inc. and are reproduced herein with
- * the permission of UNIX System Laboratories, Inc.
+ * Copyright (C) 2001-2003 by egnite Software GmbH. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY EGNITE SOFTWARE GMBH AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL EGNITE
+ * SOFTWARE GMBH OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * For additional information see http://www.ethernut.de/
+ *
+ * -
+ * Portions Copyright (c) 1983, 1993 by
+ *  The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,94 +59,63 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- *	@(#)errno.h	8.5 (Berkeley) 1/21/94
  */
 
-#ifndef _ERRNO_H_
-#define _ERRNO_H_
+/*
+ * $Log$
+ * Revision 1.3  2004/04/14 18:30:58  freckle
+ * This is Revision 1.1.1.1 of errno.h. Revision 1.2 was checked in accidently
+ *
+ * Revision 1.1.1.1  2003/05/09 14:41:11  haraldkipp
+ * Initial using 3.2.1
+ *
+ * Revision 1.5  2003/02/04 18:00:42  harald
+ * Version 3 released
+ *
+ * Revision 1.4  2002/06/26 17:29:18  harald
+ * First pre-release with 2.4 stack
+ *
+ */
 
-extern int errno;			/* global error number */
+/*!
+ * \file net/errno.h
+ * \brief Network error definitions.
+ */
 
-#define	EPERM		1		/* Operation not permitted */
-#define	ENOENT		2		/* No such file or directory */
-#define	ESRCH		3		/* No such process */
-#define	EINTR		4		/* Interrupted system call */
-#define	EIO		5		/* Input/output error */
-#define	ENXIO		6		/* Device not configured */
-#define	E2BIG		7		/* Argument list too long */
-#define	ENOEXEC		8		/* Exec format error */
-#define	EBADF		9		/* Bad file descriptor */
-#define	ECHILD		10		/* No child processes */
-#define	EDEADLK		11		/* Resource deadlock avoided */
-					/* 11 was EAGAIN */
-#define	ENOMEM		12		/* Cannot allocate memory */
-#define	EACCES		13		/* Permission denied */
-#define	EFAULT		14		/* Bad address */
-#define	ENOTBLK		15		/* Block device required */
-#define	EBUSY		16		/* Device busy */
-#define	EEXIST		17		/* File exists */
-#define	EXDEV		18		/* Cross-device link */
-#define	ENODEV		19		/* Operation not supported by device */
-#define	ENOTDIR		20		/* Not a directory */
-#define	EISDIR		21		/* Is a directory */
-#define	EINVAL		22		/* Invalid argument */
-#define	ENFILE		23		/* Too many open files in system */
-#define	EMFILE		24		/* Too many open files */
-#define	ENOTTY		25		/* Inappropriate ioctl for device */
-#define	ETXTBSY		26		/* Text file busy */
-#define	EFBIG		27		/* File too large */
-#define	ENOSPC		28		/* No space left on device */
-#define	ESPIPE		29		/* Illegal seek */
-#define	EROFS		30		/* Read-only file system */
-#define	EMLINK		31		/* Too many links */
-#define	EPIPE		32		/* Broken pipe */
+#define EINVAL          22      /*!< Invalid argument */
 
-/* math software */
-#define	EDOM		33		/* Numerical argument out of domain */
-#define	ERANGE		34		/* Result too large */
 
-/* non-blocking and interrupt i/o */
-#define	EAGAIN		35		/* Resource temporarily unavailable */
-#define	EWOULDBLOCK	EAGAIN		/* Operation would block */
-#define	EINPROGRESS	36		/* Operation now in progress */
-#define	EALREADY	37		/* Operation already in progress */
-
-/* ipc/network software -- argument errors */
-#define	ENOTSOCK	38		/* Socket operation on non-socket */
-#define	EDESTADDRREQ	39		/* Destination address required */
-#define	EMSGSIZE	40		/* Message too long */
-#define	EPROTOTYPE	41		/* Protocol wrong type for socket */
-#define	ENOPROTOOPT	42		/* Protocol not available */
-#define	EPROTONOSUPPORT	43		/* Protocol not supported */
-#define	ESOCKTNOSUPPORT	44		/* Socket type not supported */
-#define	EOPNOTSUPP	45		/* Operation not supported */
-#define	EPFNOSUPPORT	46		/* Protocol family not supported */
-#define	EAFNOSUPPORT	47		/* Address family not supported by protocol family */
-#define	EADDRINUSE	48		/* Address already in use */
-#define	EADDRNOTAVAIL	49		/* Can't assign requested address */
-
-/* ipc/network software -- operational errors */
-#define	ENETDOWN	50		/* Network is down */
-#define	ENETUNREACH	51		/* Network is unreachable */
-#define	ENETRESET	52		/* Network dropped connection on reset */
-#define	ECONNABORTED	53		/* Software caused connection abort */
-#define	ECONNRESET	54		/* Connection reset by peer */
-#define	ENOBUFS		55		/* No buffer space available */
-#define	EISCONN		56		/* Socket is already connected */
-#define	ENOTCONN	57		/* Socket is not connected */
-#define	ESHUTDOWN	58		/* Can't send after socket shutdown */
-#define	ETOOMANYREFS	59		/* Too many references: can't splice */
-#define	ETIMEDOUT	60		/* Operation timed out */
-#define	ECONNREFUSED	61		/* Connection refused */
-
-#define	ELOOP		62		/* Too many levels of symbolic links */
-#define	ENAMETOOLONG	63		/* File name too long */
-
-/* should be rearranged */
-#define	EHOSTDOWN	64		/* Host is down */
-#define	EHOSTUNREACH	65		/* No route to host */
-#define	ENOTEMPTY	66		/* Directory not empty */
+#define EWOULDBLOCK     10035   //!< Operation would block
+#define EINPROGRESS     10036   //!< Operation now in progress
+#define EALREADY        10037   //!< Operation already in progress
+#define ENOTSOCK        10038   //!< Socket operation on non-socket
+#define EDESTADDRREQ    10039   //!< Destination address required
+#define EMSGSIZE        10040   //!< Message too long
+#define EPROTOTYPE      10041   //!< Protocol wrong type for socket
+#define ENOPROTOOPT     10042   //!< Protocol not available
+#define EPROTONOSUPPORT 10043   //!< Protocol not supported
+#define ESOCKTNOSUPPORT 10044   //!< Socket type not supported
+#define EOPNOTSUPP      10045   //!< Operation not supported on socket
+#define EPFNOSUPPORT    10046   //!< Protocol family not supported
+#define EAFNOSUPPORT    10047   //!< Address family not supported by protocol family
+#define EADDRINUSE      10048   //!< Address already in use
+#define EADDRNOTAVAIL   10049   //!< Can't assign requested address
+#define ENETDOWN        10050   //!< Network is down
+#define ENETUNREACH     10051   //!< Network is unreachable
+#define ENETRESET       10052   //!< Network dropped connection on reset
+#define ECONNABORTED    10053   //!< Software caused connection abort
+#define ECONNRESET      10054   //!< Connection reset by peer
+#define ENOBUFS         10055   //!< No buffer space available
+#define EISCONN         10056   //!< Socket is already connected
+#define ENOTCONN        10057   //!< Socket is not connected
+#define ESHUTDOWN       10058   //!< Can't send after socket shutdown
+#define ETOOMANYREFS    10059   //!< Too many references: can't splice
+#define ETIMEDOUT       10060   //!< Connection timed out
+#define ECONNREFUSED    10061   //!< Connection refused
+#define ELOOP           10062   //!< Too many levels of symbolic links
+#define ENAMETOOLONG    10063   //!< File name too long
+#define EHOSTDOWN       10064   //!< Host is down
+#define EHOSTUNREACH    10065   //!< No route to host
+#define ENOTEMPTY       10066   //!< Directory not empty
 
 #endif
-
