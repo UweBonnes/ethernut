49b83de81054c19c8c2f61119182157acaa5f8f9
freckle <freckle@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2004-05-17 18:59:50 +0000

    Implemented recursive mutex and semaphore functions for nut/os based on the posix specifation using existing nut/os primitives (post/wait event)
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@350 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

---
+++ include/sys/semaphore.h
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2000-2004 by ETH Zurich
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ETH ZURICH AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ETH ZURICH
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * For additional information see http://www.ethernut.de/
+ *
+ */
+
+/* sempahore.h - a nut/os implementation of semaphore functions
+ *
+ * 2004.05.06 Matthias Ringwald <matthias.ringwald@inf.ethz.ch>
+ *
+ */
+
+#include <sys/types.h>
+#include <sys/thread.h>
+
+/*!
+ * \addtogroup xgSemaphore
+ */
+/*@{*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*!
+ * \brief Sempahore type.
+ */
+    typedef struct _SEM SEM;
+
+/*!
+ * \struct _mutex mutex.h sys/mutex.h
+ * \brief Recursive mutex.
+ *
+ */
+    struct _SEM {
+        HANDLE qhp;             /*!< \brief Queue to wait, if semaphore is zero. */
+        u_short value;          /*!< \brief semaphore value . */
+    };
+
+    extern void NutSemInit(SEM * sem, u_short value);
+    extern void NutSemWait(SEM * sem);
+    extern int NutSemTryWait(SEM * sem);
+    extern void NutSemPost(SEM * sem);
+    extern int NutSemDestroy(SEM * sem);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ os/semaphore.c
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2000-2004 by ETH Zurich
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ETH ZURICH AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ETH ZURICH
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * For additional information see http://www.ethernut.de/
+ *
+ */
+
+/* semaphore.c - a nut/os implementation of semaphore functions
+ *
+ * 2004.05.06 Matthias Ringwald <matthias.ringwald@inf.ethz.ch>
+ *
+ */
+
+/*!
+ * \addtogroup xgSemaphore
+ */
+/*@{*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <sys/semaphore.h>
+#include <sys/event.h>
+
+/*!
+ * \brief Initialize an unnamed semaphore to value
+ */
+    void NutSemInit(SEM * sem, u_short value) {
+        sem->qhp = 0;
+        sem->value = value;
+    }
+/*!
+ * \brief Lock a semaphore
+ *
+ *  If the semaphore value is currently zero, then the calling thread will not
+ *  return from the call to sem_wait() the semaphore becomes available
+ *
+ * \Note: Should not be called from interrupt context
+ */ void NutSemWait(SEM * sem) {
+        if (sem->value == 0)
+            NutEventWait(&sem->qhp, NUT_WAIT_INFINITE);
+        sem->value--;
+    }
+
+/*!
+ * \brief Unlock a sempahore.
+ *
+ * \Note: Should not be called from interrupt context
+ */
+    void NutSemPost(SEM * sem) {
+        if (sem->value++ == 0)
+            NutEventPost(&sem->qhp);
+    }
+
+/*!
+ * \brief Attempt to lock a semaphore without blocking
+ *
+ * Return zero, if successful, otherwise the sempahore is already locked
+ * \Note: Should not be called from interrupt context
+ */
+
+    int NutSemTryWait(SEM * sem) {
+        if (sem->value == 0)
+            return -1;
+        else
+            NutSemWait(sem);
+        return 0;
+    }
+
+/*!
+ * \brief Free resources allocated for a semaphore
+ *
+ * Return zero, if successful, otherwise there are threads blocked on the
+ * sempahore
+ */
+
+    int NutSemDestroy(SEM * sem) {
+        if (sem->qhp == SIGNALED)
+            return 0;
+        if (sem->qhp == 0)
+            return 0;
+        return -1;
+    }
+
+#ifdef __cplusplus
+}
+#endif
