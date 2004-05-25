5dda7e9b978746bcca6832ba37a84e012a0bab36
olereinhardt <olereinhardt@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2004-05-25 11:41:36 +0000

    newly indented according to NutOS coding style
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@367 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- dev/hd44780_bus.c
+++ dev/hd44780_bus.c
@@ -45,8 +45,8 @@
  *
  * Have a look to our m-can board if you have questions.
  */
- 
- 
+
+
 
 #include <stdlib.h>
 #include <string.h>
@@ -74,22 +74,23 @@ static u_short lcd_base = 0x0000;
 
 static inline void LcdBusyWait(void)
 {
-//	volatile u_char count;
-	// wait until LCD busy bit goes to zero
-	// do a read from control register
-	while(*(volatile u_char *) (LCD_CTRL_ADDR+LCD_READ_OFFSET) & 1<<LCD_BUSY) LCD_DELAY;
-	LCD_DELAY;
-	LCD_DELAY;
-	LCD_DELAY;
-	LCD_DELAY;
-	LCD_DELAY;
-	LCD_DELAY;
-	LCD_DELAY;
-	LCD_DELAY;
-	LCD_DELAY;
-	LCD_DELAY;
-	LCD_DELAY;
-	LCD_DELAY;
+//      volatile u_char count;
+    // wait until LCD busy bit goes to zero
+    // do a read from control register
+    while (*(volatile u_char *) (LCD_CTRL_ADDR + LCD_READ_OFFSET) & 1 << LCD_BUSY)
+        LCD_DELAY;
+    LCD_DELAY;
+    LCD_DELAY;
+    LCD_DELAY;
+    LCD_DELAY;
+    LCD_DELAY;
+    LCD_DELAY;
+    LCD_DELAY;
+    LCD_DELAY;
+    LCD_DELAY;
+    LCD_DELAY;
+    LCD_DELAY;
+    LCD_DELAY;
 }
 
 /*!
@@ -100,8 +101,8 @@ static inline void LcdBusyWait(void)
 
 static void LcdWriteData(u_char data)
 {
-	LcdBusyWait();						// wait until LCD not busy
-	*(volatile u_char *) (LCD_DATA_ADDR) = data;
+    LcdBusyWait();              // wait until LCD not busy
+    *(volatile u_char *) (LCD_DATA_ADDR) = data;
 }
 
 /*!
@@ -109,124 +110,124 @@ static void LcdWriteData(u_char data)
  */
 static void LcdWriteCmd(u_char cmd, u_char delay)
 {
-	LcdBusyWait();						// wait until LCD not busy
-	*(volatile u_char *) (LCD_CTRL_ADDR) = cmd;
+    LcdBusyWait();              // wait until LCD not busy
+    *(volatile u_char *) (LCD_CTRL_ADDR) = cmd;
 }
 
 static void LcdSetCursor(u_char pos)
 {
-	LcdWriteCmd(1<<LCD_DDRAM | pos, 0);
+    LcdWriteCmd(1 << LCD_DDRAM | pos, 0);
 }
 
 static void LcdCursorHome(void)
 {
-	LcdWriteCmd(1<<LCD_HOME, 0);
+    LcdWriteCmd(1 << LCD_HOME, 0);
 }
 
 static void LcdCursorLeft(void)
 {
-	LcdWriteCmd(1<<LCD_MOVE, 0);
+    LcdWriteCmd(1 << LCD_MOVE, 0);
 }
 
 static void LcdCursorRight(void)
 {
-	LcdWriteCmd(1<<LCD_MOVE | 1 << LCD_MOVE_RIGHT, 0);
+    LcdWriteCmd(1 << LCD_MOVE | 1 << LCD_MOVE_RIGHT, 0);
 }
 
 static void LcdClear(void)
 {
-	LcdWriteCmd(1<<LCD_CLR, 0);
+    LcdWriteCmd(1 << LCD_CLR, 0);
 }
 
 static void LcdCursorMode(u_char on)
 {
-	LcdWriteCmd(1<<LCD_ON_CTRL | on ? 1<< LCD_ON_CURSOR : 0x00, 0);
+    LcdWriteCmd(1 << LCD_ON_CTRL | on ? 1 << LCD_ON_CURSOR : 0x00, 0);
 }
 
 /*!
  * \brief Initialization of the LCD controller
  */
 
-static void LcdInit(NUTDEVICE *dev)
+static void LcdInit(NUTDEVICE * dev)
 {
-	lcd_base = dev->dev_base;
-#ifdef  KS0073_CONTROLLER	
-	// LCD function set with extended Register Set.
-	LcdWriteCmd((1 << LCD_FUNCTION) | 1<<LCD_FUNCTION_8BIT | (1<<LCD_FUNCTION_RE), 0);
-	NutDelay(50);
-	LcdWriteCmd((1 << LCD_FUNCTION) | 1<<LCD_FUNCTION_8BIT | (1<<LCD_FUNCTION_RE), 0);
-	NutDelay(50);
-	LcdWriteCmd((1 << LCD_FUNCTION) | 1<<LCD_FUNCTION_8BIT | (1<<LCD_FUNCTION_RE), 0);
-	NutDelay(50);
-	
-	LcdWriteCmd((1 << LCD_EXT) | ((((TERMDCB*)dev->dev_dcb)->dcb_nrows > 2) ?(1<<LCD_EXT_4LINES):0), 0);
-	LcdWriteCmd((1 << LCD_FUNCTION) | 1<<LCD_FUNCTION_8BIT, 0);	
-	
+    lcd_base = dev->dev_base;
+#ifdef  KS0073_CONTROLLER
+    // LCD function set with extended Register Set.
+    LcdWriteCmd((1 << LCD_FUNCTION) | 1 << LCD_FUNCTION_8BIT | (1 << LCD_FUNCTION_RE), 0);
+    NutDelay(50);
+    LcdWriteCmd((1 << LCD_FUNCTION) | 1 << LCD_FUNCTION_8BIT | (1 << LCD_FUNCTION_RE), 0);
+    NutDelay(50);
+    LcdWriteCmd((1 << LCD_FUNCTION) | 1 << LCD_FUNCTION_8BIT | (1 << LCD_FUNCTION_RE), 0);
+    NutDelay(50);
+
+    LcdWriteCmd((1 << LCD_EXT) | ((((TERMDCB *) dev->dev_dcb)->dcb_nrows > 2) ? (1 << LCD_EXT_4LINES) : 0), 0);
+    LcdWriteCmd((1 << LCD_FUNCTION) | 1 << LCD_FUNCTION_8BIT, 0);
+
 #else
-	
-	// LCD function set
-	
-	LcdWriteCmd((1 << LCD_FUNCTION) | 1<<LCD_FUNCTION_8BIT | (1<<LCD_FUNCTION_2LINES), 0);	
-	NutDelay(50);
-	LcdWriteCmd((1 << LCD_FUNCTION) | 1<<LCD_FUNCTION_8BIT | (1<<LCD_FUNCTION_2LINES), 0);	
-	NutDelay(50);
-	LcdWriteCmd((1 << LCD_FUNCTION) | 1<<LCD_FUNCTION_8BIT | (1<<LCD_FUNCTION_2LINES), 0);	
-	NutDelay(50);
-#endif		
-	// clear LCD
-	LcdWriteCmd(1<<LCD_CLR, 0);
-	// set entry mode
-	LcdWriteCmd(1<<LCD_ENTRY_MODE | 1<<LCD_ENTRY_INC, 0);
-	// set display to on
-	LcdWriteCmd(1<<LCD_ON_CTRL | 1<<LCD_ON_DISPLAY , 0);
-	// move cursor to home
-	LcdWriteCmd(1<<LCD_HOME, 0);
-	// set data address to 0
-	LcdWriteCmd(1<<LCD_DDRAM | 0x00, 0);
+
+    // LCD function set
+
+    LcdWriteCmd((1 << LCD_FUNCTION) | 1 << LCD_FUNCTION_8BIT | (1 << LCD_FUNCTION_2LINES), 0);
+    NutDelay(50);
+    LcdWriteCmd((1 << LCD_FUNCTION) | 1 << LCD_FUNCTION_8BIT | (1 << LCD_FUNCTION_2LINES), 0);
+    NutDelay(50);
+    LcdWriteCmd((1 << LCD_FUNCTION) | 1 << LCD_FUNCTION_8BIT | (1 << LCD_FUNCTION_2LINES), 0);
+    NutDelay(50);
+#endif
+    // clear LCD
+    LcdWriteCmd(1 << LCD_CLR, 0);
+    // set entry mode
+    LcdWriteCmd(1 << LCD_ENTRY_MODE | 1 << LCD_ENTRY_INC, 0);
+    // set display to on
+    LcdWriteCmd(1 << LCD_ON_CTRL | 1 << LCD_ON_DISPLAY, 0);
+    // move cursor to home
+    LcdWriteCmd(1 << LCD_HOME, 0);
+    // set data address to 0
+    LcdWriteCmd(1 << LCD_DDRAM | 0x00, 0);
 }
 
 /*!
  * \brief Terminal device control block structure.
  */
 TERMDCB dcb_term = {
-    LcdInit,            /*!< \brief Initialize display subsystem, dss_init. */
-    LcdWriteData,       /*!< \brief Write display character, dss_write. */
-    LcdWriteCmd,        /*!< \brief Write display command, dss_command. */
-    LcdClear,           /*!< \brief Clear display, dss_clear. */
-    LcdSetCursor,       /*!< \brief Set display cursor, dss_set_cursor. */
-    LcdCursorHome,      /*!< \brief Set display cursor home, dss_cursor_home. */
-    LcdCursorLeft,      /*!< \brief Move display cursor left, dss_cursor_left. */
-    LcdCursorRight,     /*!< \brief Move display cursor right, dss_cursor_right. */
-    LcdCursorMode,      /*!< \brief Switch cursor on/off, dss_cursor_mode. */
-    0,                  /*!< \brief Mode flags. */
-    0,                  /*!< \brief Status flags. */
-#ifdef  KS0073_CONTROLLER	
-    4,                  /*!< \brief Number of rows. */
-    32,                 /*!< \brief Number of columns per row. */
-    20,                 /*!< \brief Number of visible columns. */
+    LcdInit,                    /*!< \brief Initialize display subsystem, dss_init. */
+    LcdWriteData,               /*!< \brief Write display character, dss_write. */
+    LcdWriteCmd,                /*!< \brief Write display command, dss_command. */
+    LcdClear,                   /*!< \brief Clear display, dss_clear. */
+    LcdSetCursor,               /*!< \brief Set display cursor, dss_set_cursor. */
+    LcdCursorHome,              /*!< \brief Set display cursor home, dss_cursor_home. */
+    LcdCursorLeft,              /*!< \brief Move display cursor left, dss_cursor_left. */
+    LcdCursorRight,             /*!< \brief Move display cursor right, dss_cursor_right. */
+    LcdCursorMode,              /*!< \brief Switch cursor on/off, dss_cursor_mode. */
+    0,                          /*!< \brief Mode flags. */
+    0,                          /*!< \brief Status flags. */
+#ifdef  KS0073_CONTROLLER
+    4,                          /*!< \brief Number of rows. */
+    32,                         /*!< \brief Number of columns per row. */
+    20,                         /*!< \brief Number of visible columns. */
 #else
-    2,                  /*!< \brief Number of rows. */
-    80,                 /*!< \brief Number of columns per row. */
-    20,                 /*!< \brief Number of visible columns. */	
+    2,                          /*!< \brief Number of rows. */
+    80,                         /*!< \brief Number of columns per row. */
+    20,                         /*!< \brief Number of visible columns. */
 #endif
-    0,                  /*!< \brief Cursor row. */
-    0,                  /*!< \brief Cursor column. */
-    0                   /*!< \brief Display shadow memory. */
+    0,                          /*!< \brief Cursor row. */
+    0,                          /*!< \brief Cursor column. */
+    0                           /*!< \brief Display shadow memory. */
 };
 
 /*!
  * \brief LCD device information structure.
  */
 NUTDEVICE devLcdBus = {
-    0,              /*!< Pointer to next device. */
-    {'l', 'c', 'd', 'b', 'u', 's', 0, 0, 0},      /*!< Unique device name. */
-    IFTYP_STREAM,   /*!< Type of device. */
-    0,              /*!< Base address. */
-    0,              /*!< First interrupt number. */
-    0,              /*!< Interface control block. */
-    &dcb_term,      /*!< Driver control block. */
-    TermInit,       /*!< Driver initialization routine. */
-    TermIOCtl,      /*!< Driver specific control function. */
+    0,                          /*!< Pointer to next device. */
+    {'l', 'c', 'd', 'b', 'u', 's', 0, 0, 0},    /*!< Unique device name. */
+    IFTYP_STREAM,               /*!< Type of device. */
+    0,                          /*!< Base address. */
+    0,                          /*!< First interrupt number. */
+    0,                          /*!< Interface control block. */
+    &dcb_term,                  /*!< Driver control block. */
+    TermInit,                   /*!< Driver initialization routine. */
+    TermIOCtl,                  /*!< Driver specific control function. */
     0,
     TermWrite,
     TermWrite_P,
