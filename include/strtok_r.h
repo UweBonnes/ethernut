5e1602649a2f5a5bd345def996fad5df4dc06a50
drsung <drsung@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2004-07-27 19:33:52 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@412 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- include/strtok_r.h
+++ include/strtok_r.h
@@ -10,15 +10,20 @@
 
 /*
  * $Log$
+ * Revision 1.4  2004/07/27 19:29:16  drsung
+ * *** empty log message ***
+ *
  * Revision 1.3  2003/12/16 22:35:08  drsung
  * Portability issues
  *
  */
 
+#include <compiler.h>
+
 #ifdef __IMAGECRAFT__
-char * strtok_r(  char ** pp_str, const char * p_delim );
+char *strtok_r(char *s, CONST char *delim, char **save_ptr);
 #endif
-char * strsep_r(  char ** pp_str, const char * p_delim );
-char * strsep_rs( char ** pp_str, const char * p_delim, char * p_term );
+char * strsep_r(  char ** pp_str, CONST char * p_delim );
+char * strsep_rs( char ** pp_str, CONST char * p_delim, char * p_term );
 
 /*------------------------- end of file STRTOK_R.H -------------------------*/
