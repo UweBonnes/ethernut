ab4baa0772a0dbc69fd506267fd99dbab49e9ee4
drsung <drsung@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2004-07-30 19:10:42 +0000

    First proposal to define some global ioctl commands.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@419 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- include/io.h
+++ include/io.h
@@ -1,7 +1,63 @@
 #ifndef _IO_H_
 #define	_IO_H_
 
+/*
+ * Copyright (C) 2001-2004 by egnite Software GmbH. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY EGNITE SOFTWARE GMBH AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL EGNITE
+ * SOFTWARE GMBH OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * For additional information see http://www.ethernut.de/
+ */
+
+/*
+ * $Log$
+ * Revision 1.3  2004/07/30 19:10:42  drsung
+ * First proposal to define some global ioctl commands.
+ *
+ */
+
 #include <sys/types.h>
+
+/* 
+ * Some global definitions for ioctl commands.
+ */
+#define IOCTL_GETSTATUS                 0x0001
+#define IOCTL_SETSTATUS                 0x0002
+#define IOCTL_SETBUFFERMODE             0x0003
+#define IOCTL_GETBUFFERMODE             0x0004
+#define IOCTL_GETOUTBUFSIZE             0x0005
+#define IOCTL_SETOUTBUFSIZE             0x0006
+#define IOCTL_GETINBUFSIZE              0x0007
+#define IOCTL_SETINBUFSIZE              0x0008
+#define IOCTL_FLUSHOUTBUF               0x0009
+#define IOCTL_GETFILESIZE               0x000A
+#define IOCTL_GETOUTBUFCOUNT            0x000B
+#define IOCTL_GETINBUFCOUNT             0x000C
+
 
 extern int _close(int fd);
 extern int _open(CONST char *name, int mode);
