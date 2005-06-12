76e3e74ec70db999eb2d6bf1a657d62ed9dd6ca6
haraldkipp <haraldkipp@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2005-06-12 16:50:57 +0000

    Major redesign to provide better portability and reduce interrupt latency.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@937 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- arch/avr/os/timer.c
+++ arch/avr/os/timer.c
@@ -33,6 +33,9 @@
 
 /*
  * $Log$
+ * Revision 1.2  2005/06/12 16:50:57  haraldkipp
+ * Major redesign to provide better portability and reduce interrupt latency.
+ *
  * Revision 1.1  2005/05/27 17:17:31  drsung
  * Moved the file
  *
@@ -68,16 +71,92 @@
  *
  */
 
+#ifdef NUT_CPU_FREQ             /* ----- NUT_CPU_FREQ */
+#ifndef NUT_TIMER_CRYSTAL
+#define NUT_TIMER_CRYSTAL   NUT_CPU_FREQ
+#endif
+#ifndef NUT_TIMER_PRESCALE
+#define NUT_TIMER_PRESCALE  128
+#endif
+#else                           /* ----- !NUT_CPU_FREQ */
+#ifndef NUT_TIMER_CRYSTAL
+#define NUT_TIMER_CRYSTAL   32768L
+#endif
+#ifndef NUT_TIMER_PRESCALE
+#define NUT_TIMER_PRESCALE  1
+#endif
+#endif                          /* ----- NUT_CPU_FREQ */
+
+/* Output compare register value. */
+#define OCR_VALUE       ((2 * NUT_TIMER_CRYSTAL / (NUT_TIMER_PRESCALE * NUT_TICK_NFREQ) + 1) / 2)
+
+/* Calucalted number of timer ticks per second. */
+#define NUT_TICK_FREQ   ((2 * NUT_TIMER_CRYSTAL / OCR_VALUE / NUT_TIMER_PRESCALE + 1) / 2)
+
+
+#ifdef NUT_CPU_FREQ             /* ----- NUT_CPU_FREQ */
+#if defined(MCU_AT90CAN128)     /* MCU_AT90CAN128 */
+#define TCCR_FLAGS  (_BV(CS20) | _BV(CS22) | _BV(WGM21))
+#elif defined(MCU_ATMEGA103)    /* MCU_ATMEGA103 */
+#define TCCR_FLAGS  (_BV(CS00) | _BV(CS02) | _BV(CTC0))
+#else                           /* MCU_ATMEGA128 */
+#define TCCR_FLAGS  (_BV(CS00) | _BV(CS02) | _BV(WGM01))
+#endif
+#else                           /* ----- !NUT_CPU_FREQ */
+#if defined(MCU_ATMEGA103)      /* MCU_ATMEGA103 */
+#define TCCR_FLAGS  (_BV(CS00) | _BV(CTC0))
+#define TCCR_AFLAGS _BV(CS01)
+#define ASSR_BIT    AS0
+#define ASSR_BUSY   (_BV(TCN0UB) | _BV(OCR0UB) | _BV(TCR0UB))
+#elif defined(MCU_ATMEGA128)    /* MCU_ATMEGA128 */
+#define TCCR_FLAGS  (_BV(CS00) | _BV(WGM01))
+#define TCCR_AFLAGS _BV(CS01)
+#define ASSR_BIT    AS0
+#define ASSR_BUSY   (_BV(TCN0UB) | _BV(OCR0UB) | _BV(TCR0UB))
+#else                           /* Other MCU */
+#error Define NUT_CPU_FREQ to compile for this CPU.
+#endif
+#endif                          /* ----- NUT_CPU_FREQ */
+
+#ifdef MCU_AT90CAN128
+#define TCCRx       TCCR2A
+#define TCNTx       TCNT2
+#define OCRx        OCR2A
+#define TIFR_OCFx   _BV(OCF2A)
+#define TIFR_TOVx  _BV(TOV2)
+#define sig_TIMER   sig_OUTPUT_COMPARE2
+#else
+#define TCCRx       TCCR0
+#define TCNTx       TCNT0
+#define OCRx        OCR0
+#define TIFR_OCFx   _BV(OCF0)
+#define TIFR_TOVx  _BV(TOV0)
+#define sig_TIMER   sig_OUTPUT_COMPARE0
+#endif
+
+#ifdef MCU_AT90CAN128
+#define NutEnableTimerIrq()     sbi(TIMSK2, OCIE2A)
+#define NutDisableTimerIrq()    cbi(TIMSK2, OCIE2A)
+#else
+#define NutEnableTimerIrq()     sbi(TIMSK, OCIE0)
+#define NutDisableTimerIrq()    cbi(TIMSK, OCIE0)
+#endif
+
+static u_long cpu_clock;
+
+
 /*!
  * \brief Loop for a specified number of milliseconds.
  *
- * This call will not release the CPU and will
- * not switch to another thread. However, because
- * of absent thread switching, this delay time is
- * very exact.
+ * This call will not release the CPU and will not switch to another 
+ * thread. However, because of absent thread switching, this delay 
+ * time is very exact.
  *
- * Use NutSleep() to avoid blocking the CPU, if no
- * exact timing is needed.
+ * If no exact timing is needed, use NutSleep() to avoid blocking the 
+ * CPU, 
+ *
+ * \note The timing may not work as expected. A better implementation
+ *       is urgently required.
  *
  * \param ms Delay time in milliseconds, maximum is 255.
  */
@@ -85,11 +164,17 @@ void NutDelay(u_char ms)
 {
 #ifdef __GNUC__
     u_short cnt;
-    asm volatile ("\n"
-                  "L_dl1%=:\n\t"
-                  "mov %A0, %A2\n\t"
-                  "mov %B0, %B2\n" "L_dl2%=:\n\t" "sbiw %A0, 1\n\t" "brne L_dl2%=\n\t" "dec %1\n\t" "brne L_dl1%=\n\t":"=&w" (cnt)
-                  :"r"(ms), "r"((u_short) (delay_count))
+    __asm__ __volatile__("L_dl1%=:     \n\t"    /* */
+                         "mov %A0, %A2 \n\t"    /* */
+                         "mov %B0, %B2 \n\n"    /* */
+                         "L_dl2%=:     \n\t"    /* */
+                         "sbiw %A0, 1  \n\t"    /* */
+                         "brne L_dl2%= \n\t"    /* */
+                         "dec %1       \n\t"    /* */
+                         "brne L_dl1%=     "    /* */
+                         :"=&w"(cnt)    /* Output %0 */
+                         :"r"(ms)       /* Input %1 */
+                         , "r"((u_short) (cpu_clock / 4000))    /* Input %2 */
         );
 #else
     u_short delay_cnt = 2400;   //*KU* for 14.745600 MHz Clock
@@ -102,216 +187,69 @@ void NutDelay(u_char ms)
 #endif
 }
 
-/*
- * Timer interrupt handler. Timer0 on ATmega's and Timer2 on AT90CAN128 parts.
+/*! \fn CountCpuLoops(void)
+ * \brief Count the number of code loops until timer overflows.
+ *
+ * \return The number of loops.
  */
-static void NutTimerIntr(void *arg)
-{
-    NUTTIMERINFO *tnp;
-
-#ifdef NUTTRACER
-    TRACE_ADD_ITEM(TRACE_TAG_INTERRUPT_ENTER,TRACE_INT_TIMER0_OVERFL);
-#endif
-
-    /*
-     * Increment the tick counter used by Michael Fischer's
-     * NutGetTickCount() routine.
-     */
-    milli_ticks++;
-
-#ifdef NUT_CPU_FREQ
-
-    millis++;
-    ms1++; /* See comment on volatiles below. */
-    if (ms1 >= 1000) {
-        ms1 = 0;
-        seconds++;
-    }
-#else                           /* NUT_CPU_FREQ */
-
-    if (ms62_5 & 1) {
-        millis += 62;
-    } else {
-        millis += 63;
-    }
-    /*
-     * Update RTC. We do a post increment here, because
-     * of an ImageCraft bug with volatiles. Thanks to
-     * Michael Fischer.
-     *
-     * Later comment: Now I'm confused. The ICCAVR bug was reported on
-     * pre-increments only. But later Michael Fischer sent a fix for
-     * twi, where all increments of volatile variables had been moved
-     * out of the comparision. Checking all of the code again for these
-     * increments I stumbled over this one, which had been fixed
-     * previously by changing the pre-increment to a post-increment.
-     *
-     * To play save, I'll follow Michael's advice and isolate all
-     * increments of volatile variables from expressions.
-     */
-    ms62_5++;
-    if (ms62_5 >= 16) {
-        ms62_5 = 0;
-        seconds++;
-    }
-#endif                          /* NUT_CPU_FREQ */
-
-    if (nutTimerList) {
-        if (nutTimerList->tn_ticks_left)
-            nutTimerList->tn_ticks_left--;
-
-        /*
-         * Process all elapsed timers.
-         * Bugfix by KU: Avoid crash on empty timer list.
-         */
-        while ((nutTimerList != 0) && (nutTimerList->tn_ticks_left == 0)) {
-            /*
-             * Execute the callback.
-             */
-            if (nutTimerList->tn_callback)
-                (*nutTimerList->tn_callback) (nutTimerList, (void *) nutTimerList->tn_arg);
-
-            /*
-             * Remove the timer from the list.
-             */
-            tnp = nutTimerList;
-            //fprintf(__os_trs, "RemTmr<%04X>\r\n", tnp);
-            nutTimerList = nutTimerList->tn_next;
-
-            /*
-             * Periodic timers are re-inserted.
-             */
-            if ((tnp->tn_ticks_left = tnp->tn_ticks) != 0)
-                NutTimerInsert(tnp);
-
-            /*
-             * We can't touch the heap while running in
-             * interrupt context. Oneshot timers are added
-             * to a pool of available timers.
-             */
-            else {
-                tnp->tn_next = nutTimerPool;
-                nutTimerPool = tnp;
-                //NutKDumpTimerList();
-            }
-        }
-    }
-#ifdef NUTTRACER
-    TRACE_ADD_ITEM(TRACE_TAG_INTERRUPT_EXIT,TRACE_INT_TIMER0_OVERFL)
-#endif
-}
-
-
 #ifndef NUT_CPU_FREQ
-
-#ifdef ARTHERNET1
-/* ARTHERNET extension - since the old method depends
-   on cycle time of the inline-assembler instructions. Fails if
-   all RAM is external (waitstaits).
-   TODO: change to Nut/OS coding style */
-   
-/*
-   Utiltiy Function - Wait one second at 20MHz clock 
-   rough! - do not use for precise delays
-*/
-static void NutComputeCpuClockDelay(void)
+static u_long CountCpuLoops(void)
 {
-	volatile u_short i, j;
-	
-	for (i = 0; i < 1000; i++) {
-		for (j = 0; j < 1000; j++);
-	}
+#ifdef __GNUC__
+    u_long rc = 1;
+
+    __asm__ __volatile__("firstovf:              \n\t"  /* */
+                         "in %D0,%1              \n\t"  /* */
+                         "andi %D0,%2            \n\t"  /* */
+                         "breq firstovf          \n\t"  /* */
+                         "out %1,%D0             \n\n"  /* */
+                         /* This loop has 8 cycles. */
+                         "nextovf:               \n\t"  /* */
+                         "sec                    \n\t"  /* */
+                         "adc %A0,__zero_reg__   \n\t"  /* */
+                         "adc %B0,__zero_reg__   \n\t"  /* */
+                         "adc %C0,__zero_reg__   \n\t"  /* */
+                         "in %D0,%1              \n\t"  /* */
+                         "andi %D0,%2            \n\t"  /* */
+                         "breq nextovf           \n\t"  /* */
+                         "clr %D0                    "  /* */
+                         :"=d"(rc)      /* Output %0 */
+                         :"I"(_SFR_IO_ADDR(TIFR))       /* Input %1 */
+                         , "I"(TIFR_TOVx)      /* Input %2 */
+                         , "0"(rc)      /* Input %0 */
+        );
+    return rc;
+#elif defined(__IMAGECRAFT__)
+    u_long rc;
+
+    asm("CLR  R0");
+    asm("CLR  R16");
+    asm("CLR  R17");
+    asm("CLR  R18");
+    asm("firstovf:");
+    asm("IN   R19, 0x36");
+    asm("ANDI R19,1");
+    asm("BREQ firstovf");
+    asm("OUT  0x36, R19");
+    /* This loop has 8 cycles. */
+    asm("nextovf:");
+    asm("SEC");
+    asm("ADC  R16, R0");
+    asm("ADC  R17, R0");
+    asm("ADC  R18, R0");
+    asm("IN   R19, 0x36");
+    asm("ANDI R19, 1");
+    asm("BREQ nextovf");
+    asm("CLR  R19");
+    asm("STD  Y+0, R16");
+    asm("STD  Y+1, R17");
+    asm("STD  Y+2, R18");
+    asm("STD  Y+3, R19");
+
+    return rc;
+#endif
 }
-
-/* 
-   Function determines CPU-Frequency by comparison
-   with 32kHz watch-crystal
-   methode: 
-   - let timer0 run in async-mode (32kHz), prescaler 1, 10 timer (compare match)
-   - let timer1 run wiht prescaler 8
-   calculation:
-        delta_T0 = delta_T1
-   cnt0*pres0/f0 = cnt1*pres1/f1
-   with: cnt0=10, pres0=0, f0=32867Hz, pres1=8
-         cnt1 measured
-   ==> f1 = cnt1*pres1*f0/cnt0
-*/
-static u_long NutComputeCpuClock(void)
-{
-    u_char sreg, cnt0;
-    u_short cnt1;
-    
-    sreg=SREG;
-    cli(); 
-    
-    TIMSK &= ~( (1<<OCIE0) | (1<<TOIE0) );  /* 1. disable Timer0 interrupts */
-    ASSR |= (1<<AS0);  /* 2. select 32,768kHz clock source */
-    
-    NutComputeCpuClockDelay(); /* 2b. stabilize  */
-    
-    TCNT0 = 0;  /* 3. write new values to TCNT, OCR, TCCR */
-    OCR0  = 30;  /* set compare match to 10 */
-    TCCR0 = (1<<CS00); /* prescaler 1 */
-    
-    /* 4. switch to async. operation */
-    while ( (ASSR & (1<<TCN0UB)) || (ASSR & (1<<OCR0UB)) || (ASSR & (1<<TCR0UB)) ) ; 
-
-    /* setup 16-bit timer 1 */
-    TCCR1B = (1<<CS11); /* prescaler 8 */
-    
-    TIFR |= ( (1<<OCF0) | (1<<TOV0) );  /* 5. clear interrupts flags */
-    
-    TCNT0 = 0; /* reset Timer0 Counter */
-    while ( (ASSR & (1<<TCN0UB)) ); /* sync */
-    
-    TCNT1 = 0; /* reset Timer1 Counter */
-    
-    while ( !(TIFR & (1<<OCF0)) ); /* wait for compare-match */
-
-    cnt0 = TCNT0;
-    cnt1 = TCNT1;
-    
-    TIFR |= ( (1<<OCF0) | (1<<TOV0) ); /* clear interrupt flags */
-    TCCR1B = 0; /* disable TC1 */
-
-    cpu_clock = cnt1 * 8 * 32786 / cnt0;
-    delay_count = cpu_clock / 4000;
-
-    SREG = sreg; /* restore status-reg. */
-    
-    
-    /* the following lines have been copied from the orignal subroutine */
-     /*
-     * Disable timer 0 interrupts.
-     */
-    cbi(TIMSK, TOIE0);
-    cbi(TIMSK, OCIE0);
-
-    /*
-     * Select oscillator 1 (32.768 kHz).
-     */
-    sbi(ASSR, AS0);
-
-    /*
-     * Reset counter register and set
-     * prescaler to 8.
-     */
-    outp(0, TCNT0);
-    outp(2, TCCR0);
-
-    /*
-     * Wait for update busy clear and
-     * enable timer overflow interrupt.
-     */
-    NutRegisterIrqHandler(&sig_OVERFLOW0, NutTimerIntr, 0);
-    while (inp(ASSR) & (TCN0UB | TCR0UB))
-        continue;
-    
-    return cpu_clock;
-}
-
-#else /* !Arthernet */
-
+#endif
 
 /*!
  * \brief Compute CPU clock in Hertz.
@@ -322,125 +260,107 @@ static u_long NutComputeCpuClock(void)
  * \return CPU clock in Hertz.
  *
  */
+#ifndef NUT_CPU_FREQ
 static u_long NutComputeCpuClock(void)
 {
-    /*
-     * Disable timer 0 interrupts.
-     */
-    cbi(TIMSK, TOIE0);
-    cbi(TIMSK, OCIE0);
+    u_long rc;
 
-    /*
-     * Select oscillator 1 (32.768 kHz).
-     */
-    sbi(ASSR, AS0);
+    /* Disable timer interrupts. */
+    NutDisableTimerIrq();
 
-    /*
-     * Reset counter register and set
-     * prescaler to 8.
-     */
-    outp(0, TCNT0);
-    outp(2, TCCR0);
+    /* Select asynchronous oscillator. */
+    sbi(ASSR, ASSR_BIT);
 
-    /*
-     * Wait for update busy clear and
-     * enable timer overflow interrupt.
-     */
-    NutRegisterIrqHandler(&sig_OVERFLOW0, NutTimerIntr, 0);
-    while (inp(ASSR) & (TCN0UB | TCR0UB))
-        continue;
-    sbi(TIMSK, TOIE0);
+    /* Reset counter register. */
+    outb(TCNTx, 0);
 
-    /*
-     * Atmel's instruction set manual was wrong.
-     * LDS takes two cycles only, not three.
-     * Thanks to Les Grant.
-     */
-#ifdef __IMAGECRAFT__
-    cpu_clock = 14745600UL / 160UL;
-#else
-    asm volatile ("ldi r24,lo8(16)" "\n\t"
-                  "eor r25,r25" "\n\t"
-                  "eor r26,r26" "\n"
-                  "L_w2%=:" "\n\t"
-                  "lds r27, ms62_5" "\n\t"
-                  "cpi r27, lo8(2)" "\n\t"
-                  "brlo L_w2%=" "\n"
-                  "L_w4%=:" "\n\t"
-                  "nop" "\n\t"
-                  "nop" "\n\t"
-                  "adiw r24,1" "\n\t"
-                  "adc r26,__zero_reg__" "\n\t"
-                  "lds r27, ms62_5" "\n\t"
-                  "cpi r27, lo8(3)" "\n\t"
-                  "brlo L_w4%=" "\n\t"
-                  "sts cpu_clock,r24" "\n\t"
-                  "sts (cpu_clock)+1,r25" "\n\t" "sts (cpu_clock)+2,r26" "\n\t":::"r24", "r25", "r26", "r27", "memory");
-#endif
-    cbi(TIMSK, TOIE0);
+    /* Set prescaler to 8. Overflow will occur every 62.5 ms. */
+    outb(TCCRx, TCCR_AFLAGS);
 
-    cpu_clock *= 160UL;
-    delay_count = cpu_clock / 4000;
+    /* Wait for asynchronous busy clear. */
+    while ((inb(ASSR) & ASSR_BUSY) != 0);
 
-    return cpu_clock;
+    /* Clear interrupt flag. */
+    outb(TIFR, TIFR_TOVx);
+
+    NutEnterCritical();
+    rc = CountCpuLoops();
+    NutExitCritical();
+
+    return rc << 7;
 }
-#endif /* !Arthernet */
-
 #endif
 
 /*!
- * \brief Initialize system timer.
+ * \brief Initialize system timer hardware.
  *
- * This function is automatically called by Nut/OS
- * during system initialization.
+ * This function is automatically called by Nut/OS during system 
+ * initialization.
  *
- * Nut/OS uses on-chip timer 0 for it's timer services.
- * Applications should not modify any registers of this
- * timer, but make use of the Nut/OS timer API. Timer 1
- * and timer 2 are available to applications.
- *
- * On AT90CAN128 processors Nut/OS uses timer 2 for it's timer services.
- * Timer 0, 1 and 3 are available to applications.
+ * \param handler System timer interrupt handler.
  */
-void NutTimerInit(void)
+void NutRegisterTimer(void (*handler) (void *))
 {
 #ifdef NUT_CPU_FREQ
-#ifdef __AVR_AT90CAN128__
-    /*
-     * On ATCAN128 Timer2 provides the functionality which Timer0 of
-     * ATmega128 offers, hence we must use Timer2 on this device.
-     * Using above calculation and prescaler setting the error is:
-     *  0.17% for 14.7456MHz
-     *  0.27% for 12.0MHz
-     */
-    outp(BV(CS20) | BV(CS22) | BV(WGM21), TCCR2A);
-    outp(0, TCNT2);
-    outp((NUT_CPU_FREQ / (128L * 1000) + 0.5/*round*/), OCR2A);
-    NutRegisterIrqHandler(&sig_OUTPUT_COMPARE2, NutTimerIntr, 0);
-    sbi(TIMSK2, OCIE2A);
+    cpu_clock = NUT_CPU_FREQ;
 #else
-    /*
-     * - Program prescaler to output PCK0/32 and clear Timer0 on compare match.
-     * - Clear timer counter register to get the first tick right.
-     * - Write (CPU frequency / (prescaler * 1KHz)) in the output compare.
-     *   register, so we'll get a compare match interrupt every millisecond.
-     */
-#if __AVR_ENHANCED__
-    outp(BV(CS00) | BV(CS02) | BV(WGM01), TCCR0);
-#else
-    outp(BV(CS00) | BV(CS02) | BV(CTC0), TCCR0);
+    cpu_clock = NutComputeCpuClock();
 #endif
-    outp(0, TCNT0);
-    outp((NUT_CPU_FREQ / (128L * 1000) + 0.5/*round*/), OCR0);
-    NutRegisterIrqHandler(&sig_OUTPUT_COMPARE0, NutTimerIntr, 0);
-    sbi(TIMSK, OCIE0);
+
+    /* Disable timer interrupts. */
+    NutDisableTimerIrq();
+
+#ifndef NUT_CPU_FREQ
+    /* Select asynchronous oscillator. */
+    sbi(ASSR, ASSR_BIT);
 #endif
-#else /* #ifdef NUT_CPU_FREQ */
-#ifdef __AVR_AT90CAN128__
-#error Define NUT_CPU_FREQ to compile for this CPU. NutComputeCpuClock not yet implemented!
-#else
-    NutComputeCpuClock();
-    sbi(TIMSK, TOIE0);
+
+    /* Reset counter register. */
+    outb(TCNTx, 0);
+
+    /* Set CTC mode and prescaler. */
+    outb(TCCRx, TCCR_FLAGS);
+
+    /* Set output compare register. */
+    outb(OCRx, OCR_VALUE);
+
+#ifndef NUT_CPU_FREQ
+    /* Wait for asynchronous busy clear. */
+    while ((inb(ASSR) & ASSR_BUSY) != 0);
 #endif
-#endif
+
+    /* Clear interrupt flags. */
+    outb(TIFR, TIFR_OCFx);
+
+    /* Enable timer compare interrupts. */
+    NutRegisterIrqHandler(&sig_TIMER, handler, 0);
 }
+
+/*!
+ * \brief Return the CPU clock in Hertz.
+ *
+ * \return CPU clock frequency in Hertz.
+ */
+u_long NutGetCpuClock(void)
+{
+    return cpu_clock;
+}
+
+/*!
+ * \brief Return the number of system ticks per second.
+ *
+ * \return System tick frequency in Hertz.
+ */
+u_long NutGetTickClock(void)
+{
+    return NUT_TICK_FREQ;
+}
+
+/*!
+ * \brief Calculate system ticks for a given number of milliseconds.
+ */
+u_long NutTimerMillisToTicks(u_long ms)
+{
+    return ms * 1000L / NutGetTickClock();
+}
+
