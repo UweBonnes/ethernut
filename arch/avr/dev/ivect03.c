ede2175567823ac47c7474a71058ae7cdb8305ec
haraldkipp <haraldkipp@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2005-07-26 18:02:41 +0000

    Moved from dev.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@1049 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

---
+++ arch/avr/dev/ivect03.c
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2001-2003 by egnite Software GmbH. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY EGNITE SOFTWARE GMBH AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL EGNITE
+ * SOFTWARE GMBH OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * For additional information see http://www.ethernut.de/
+ *
+ */
+
+/*
+ * $Log$
+ * Revision 1.1  2005/07/26 18:02:27  haraldkipp
+ * Moved from dev.
+ *
+ * Revision 1.2  2004/01/30 17:02:19  drsung
+ * Separate interrupt stack for avr-gcc only added.
+ *
+ * Revision 1.1.1.1  2003/05/09 14:40:41  haraldkipp
+ * Initial using 3.2.1
+ *
+ * Revision 1.2  2003/03/31 14:53:06  harald
+ * Prepare release 3.1
+ *
+ */
+
+#include <dev/irqreg.h>
+
+/*!
+ * \addtogroup xgIrqReg
+ */
+/*@{*/
+
+IRQ_HANDLER sig_INTERRUPT2;
+
+/*! \fn SIG_INTERRUPT2(void)
+ * \brief External interrupt 2 entry.
+ */
+#ifdef __IMAGECRAFT__
+#pragma interrupt_handler SIG_INTERRUPT2:iv_INT2
+#endif
+NUTSIGNAL(SIG_INTERRUPT2, sig_INTERRUPT2)
+
+
+/*@}*/
---
+++ arch/avr/dev/ivect35.c
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2005 by egnite Software GmbH. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY EGNITE SOFTWARE GMBH AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL EGNITE
+ * SOFTWARE GMBH OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * For additional information see http://www.ethernut.de/
+ *
+ */
+
+/*
+ * $Log$
+ * Revision 1.1  2005/07/26 18:02:40  haraldkipp
+ * Moved from dev.
+ *
+ * Revision 1.1  2005/02/10 07:06:18  hwmaier
+ * Changes to incorporate support for AT90CAN128 CPU
+ *
+ */
+
+#include <dev/irqreg.h>
+
+/*!
+ * \addtogroup xgIrqReg
+ */
+/*@{*/
+
+#if defined(SIG_SIG_CANIT) || defined(iv_CAN_TRANSFER)
+
+IRQ_HANDLER sig_CAN_TRANSFER;
+
+/*! \fn SIG_SIG_CANIT(void)
+ * \brief Store program memory interrupt entry.
+ */
+#ifdef __IMAGECRAFT__
+#pragma interrupt_handler SIG_SIG_CANIT:iv_CAN_TRANSFER
+#endif
+NUTSIGNAL(SIG_SIG_CANIT, sig_CAN_TRANSFER)
+
+#endif
+
+/*@}*/
