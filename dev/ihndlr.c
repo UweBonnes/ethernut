cdcfd0ce8e5d7a58cc7b95d8a471ed40e3e46e50
haraldkipp <haraldkipp@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2005-07-26 16:30:58 +0000

    Copyright added. Temporarily exclude NutRegisterIrqHandler() from Linux builds.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@1038 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- dev/ihndlr.c
+++ dev/ihndlr.c
@@ -1,6 +1,50 @@
+/*
+ * Copyright (C) 2001-2005 by egnite Software GmbH. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY EGNITE SOFTWARE GMBH AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL EGNITE
+ * SOFTWARE GMBH OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * For additional information see http://www.ethernut.de/
+ *
+ */
+
+/*
+ * $Log$
+ * Revision 1.2  2005/07/26 16:30:58  haraldkipp
+ * Copyright added.
+ * Temporarily exclude NutRegisterIrqHandler() from Linux builds.
+ *
+ */
+
 #include <sys/atom.h>
 #include <dev/irqreg.h>
 
+/*!
+ * \brief Call a registered interrupt handler.
+ */
 void CallHandler(IRQ_HANDLER * irh)
 {
     irh->ir_count++;
@@ -20,6 +64,7 @@ void CallHandler(IRQ_HANDLER * irh)
  *
  * \return 0 on success, -1 otherwise.
  */
+#if !defined (__linux__) && !defined(__APPLE__) && !defined(__CYGWIN__)
 int NutRegisterIrqHandler(IRQ_HANDLER * irq, void (*handler) (void *),
                           void *arg)
 {
@@ -32,3 +77,4 @@ int NutRegisterIrqHandler(IRQ_HANDLER * irq, void (*handler) (void *),
 
     return 0;
 }
+#endif
