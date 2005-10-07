68071eb6ac4994a20d68266fe4c9addb252bec90
hwmaier <hwmaier@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2005-10-07 21:44:22 +0000

    CAN_SetSpeed function added. Baudrates moved here.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@1115 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- include/dev/can_dev.h
+++ include/dev/can_dev.h
@@ -42,6 +42,9 @@
 
 /*
  * $Log$
+ * Revision 1.5  2005/10/07 21:44:22  hwmaier
+ * CAN_SetSpeed function added. Baudrates moved here.
+ *
  * Revision 1.4  2004/08/25 15:48:21  olereinhardt
  * Added function to set an acceptance filter
  *
@@ -67,18 +70,34 @@
 #define CAN_IF_2A    0x01
 #define CAN_IF_2B    0x02
 
+
+/*
+ * CAN Baud rate constants
+ */
+#define CAN_SPEED_10K      0   ///< 10 kbit/s, max. cable length 5000 m
+#define CAN_SPEED_20K      1   ///< 20 kbit/s, max. cable length 2500 m
+#define CAN_SPEED_50K      2   ///< 50 kbit/s, max. cable length 1000 m
+#define CAN_SPEED_100K     3   ///< 100 kbit/s, max. cable length 600 m
+#define CAN_SPEED_125K     4   ///< 125 kbit/s, max. cable length 500 m
+#define CAN_SPEED_250K     5   ///< 250 kbit/s, max. cable length 250 m
+#define CAN_SPEED_500K     6   ///< 500 kbit/s, max. cable length 100 m
+#define CAN_SPEED_800K     7   ///< 800 kbit/s, max. cable length 50 m
+#define CAN_SPEED_1M       8   ///< 1 Mbit/s, max. cable length 25 m
+#define CAN_SPEED_CUSTOM 255
+
+
 /*!
  * \struct _CANFRAME can_dev.h
  * \brief CAN frame structure
  */
- 
+
 struct _CANFRAME {              // todo: Implement flags
     u_long id;                  // Identifier
     u_char len;                 // Length of frame, max = 8
     u_char byte[8];
-    u_char ext;                 // Boolean, extendet frame      
+    u_char ext;                 // Boolean, extendet frame
     u_char rtr;                 // Boolean, remote transmition bit
-}; 
+};
 
 /*!
  * \brief CAN frame type
@@ -116,14 +135,14 @@ struct ifcan {
     u_long can_baudrate;        /*!< \brief Baudrate of device */
     u_char can_acc_mask[4];     /*!< \brief Acceptance mask */
     u_char can_acc_code[4];     /*!< \brief Acceptance code */
-    
+
     u_char (*can_rxavail) (NUTDEVICE *);          /*!< \brief Receive buffer data available? */
     u_char (*can_txfree) (NUTDEVICE *);           /*!< \brief Transmit buffer free? */
     void   (*can_recv) (NUTDEVICE *, CANFRAME *); /*!< \brief Receive routine. */
     void   (*can_send) (NUTDEVICE *, CANFRAME *); /*!< \brief Send routine. */
-    void   (*can_set_ac) (NUTDEVICE *, u_char*); /*!< \brief Set accaptance code */
-    void   (*can_set_am) (NUTDEVICE *, u_char*); /*!< \brief Set accaptance mask */
-    u_char (*can_set_baud) (NUTDEVICE *, u_long); /*!< \brief Set accaptance code */
+    void   (*can_set_ac) (NUTDEVICE *, u_char*);  /*!< \brief Set accaptance code */
+    void   (*can_set_am) (NUTDEVICE *, u_char*);  /*!< \brief Set accaptance mask */
+    u_char (*can_set_baud) (NUTDEVICE *, u_long); /*!< \brief Set speed */
 };
 
 /*!
@@ -131,6 +150,7 @@ struct ifcan {
  */
 typedef struct ifcan IFCAN;
 
+u_char CAN_SetSpeed(NUTDEVICE *dev, u_long baudrate);
 void   CAN_SetFilter(NUTDEVICE *dev, u_char *ac, u_char *am);
 
 void   CAN_TxFrame(NUTDEVICE *dev, CANFRAME *frame);
