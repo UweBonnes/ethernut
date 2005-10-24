c10a2978c49e00162d970550caddf82f7be2e1ba
haraldkipp <haraldkipp@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2005-10-24 11:41:39 +0000

    Initial check-in.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@1179 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

---
+++ include/dev/dm9000e.h
@@ -0,0 +1,20 @@
+#ifndef _DEV_DM9000E_H_
+#define _DEV_DM9000E_H_
+
+#include <sys/device.h>
+
+/*
+ * Available drivers.
+ */
+extern NUTDEVICE devDM9000E;
+
+#ifndef DEV_ETHER
+#define DEV_ETHER   devDM9000E
+#endif
+
+#ifndef devEth0
+#define devEth0   devDM9000E
+#endif
+
+#endif
+
