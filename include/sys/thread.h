6c36852140588059192a858aa0425a2fad8554bd
haraldkipp <haraldkipp@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2006-06-28 14:35:31 +0000

    Event/thread/timer re-design. Code indention corrected. Volatile attribute removed from THREADINFO link pointer.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@1510 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- include/sys/thread.h
+++ include/sys/thread.h
@@ -2,7 +2,7 @@
 #define _SYS_THREAD_H_
 
 /*
- * Copyright (C) 2001-2005 by egnite Software GmbH. All rights reserved.
+ * Copyright (C) 2001-2006 by egnite Software GmbH. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -49,8 +49,18 @@
  * "copying-liquorice.txt" for details.
  */
 
-/*
+/*!
+ * \file sys/thread.h
+ * \brief Thread management definitions.
+ *
+ * \verbatim
+ *
  * $Log$
+ * Revision 1.13  2006/06/28 14:35:31  haraldkipp
+ * Event/thread/timer re-design.
+ * Code indention corrected.
+ * Volatile attribute removed from THREADINFO link pointer.
+ *
  * Revision 1.12  2006/03/16 15:25:34  haraldkipp
  * Changed human readable strings from u_char to char to stop GCC 4 from
  * nagging about signedness.
@@ -98,18 +108,10 @@
  * Revision 1.12  2002/06/26 17:29:29  harald
  * First pre-release with 2.4 stack
  *
+ * \endverbatim
  */
 
 #include <sys/types.h>
-
-/*!
- * \file sys/thread.h
- * \brief Thread management definitions.
- */
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 #define DEADBEEF    0xDEADBEEF
 
@@ -122,35 +124,38 @@ extern "C" {
 /*!
  * Thread information structure type.
  */
-    typedef struct _NUTTHREADINFO NUTTHREADINFO;
+typedef struct _NUTTHREADINFO NUTTHREADINFO;
 
 /*!
  * \struct _NUTTHREADINFO thread.h sys/thread.h
  * \brief Thread information structure.
+ *
+ * \todo Sort items while considering alignment.
  */
 
-    struct _NUTTHREADINFO {
-        NUTTHREADINFO *volatile td_next;        /*!< \brief Linked list of all threads. */
-        NUTTHREADINFO *td_qnxt; /*!< \brief Linked list of all queued thread. */
-        char td_name[9];        /*!< \brief Name of this thread. */
-        u_char td_state;        /*!< \brief Operating state. One of TDS_ */
-        uptr_t td_sp;           /*!< \brief Stack pointer. */
-        u_char td_priority;     /*!< \brief Priority level. 0 is highest priority. */
-        u_char *td_memory;      /*!< \brief Pointer to heap memory used for stack. */
-        HANDLE td_timer;        /*!< \brief Event timer. */
-        HANDLE td_queue;        /*!< \brief Root entry of the waiting queue. */
+struct _NUTTHREADINFO {
+    NUTTHREADINFO *td_next;     /*!< \brief Linked list of all threads. */
+    NUTTHREADINFO *td_qnxt;     /*!< \brief Linked list of all queued thread. */
+    volatile u_int td_qpec;     /*!< \brief Pending event counter. */
+    char td_name[9];            /*!< \brief Name of this thread. */
+    u_char td_state;            /*!< \brief Operating state. One of TDS_ */
+    uptr_t td_sp;               /*!< \brief Stack pointer. */
+    u_char td_priority;         /*!< \brief Priority level. 0 is highest priority. */
+    u_char *td_memory;          /*!< \brief Pointer to heap memory used for stack. */
+    HANDLE td_timer;            /*!< \brief Event timer. */
+    volatile HANDLE td_queue;   /*!< \brief Root entry of the waiting queue. */
 #if defined (__APPLE__) || defined(__linux__) || defined(__CYGWIN__)
-        pthread_t td_pthread;   /*!< \brief pthread for unix emulations. */
-        void (*td_fn) (void *); /*!< \brief thread function */
-        void *td_arg;           /*!< \brief args given to NutCreateThread */
-        pthread_cond_t td_cv;   /*!< \brief conditional variable for unix emulations. */
-        u_short td_cs_level;    /*! \brief number critical sections has been entered without leaving */
+    pthread_t td_pthread;       /*!< \brief pthread for unix emulations. */
+    void (*td_fn) (void *);     /*!< \brief thread function */
+    void *td_arg;               /*!< \brief args given to NutCreateThread */
+    pthread_cond_t td_cv;       /*!< \brief conditional variable for unix emulations. */
+    u_short td_cs_level;        /*! \brief number critical sections has been entered without leaving */
 #endif
-    };
+};
 /*@}*/
 
 /*!
- * \addtogroup xgThread
+ * \name Thread States
  */
 /*@{*/
 #define TDS_TERM        0       /*!< Thread has exited. */
@@ -161,36 +166,36 @@ extern "C" {
 
 #define SLEEP_MODE_NONE 0xff
 
-    extern NUTTHREADINFO * runningThread;
-    extern NUTTHREADINFO * nutThreadList;
-    extern NUTTHREADINFO *volatile readyQueue;
-    extern NUTTHREADINFO * runQueue;
+extern NUTTHREADINFO *runningThread;
+extern NUTTHREADINFO *nutThreadList;
+extern NUTTHREADINFO *runQueue;
 
-
+__BEGIN_DECLS
+/* Function prototypes. */
 #if defined (__APPLE__) || defined(__linux__) || defined(__CYGWIN__)
-    extern void NutThreadInit(void);
+extern void NutThreadInit(void);
 #endif
 
 #if defined(__GNUC__) && defined (__AVR_ENHANCED__)
-    extern void NutThreadSetSleepMode(u_char mode);
+extern void NutThreadSetSleepMode(u_char mode);
 #endif
 
-    extern HANDLE NutThreadCreate(char * name, void (*fn) (void *), void *arg, size_t stackSize);
-    extern u_char NutThreadSetPriority(u_char level);
+extern HANDLE NutThreadCreate(char *name, void (*fn) (void *), void *arg, size_t stackSize);
+extern u_char NutThreadSetPriority(u_char level);
 
-    extern void NutThreadKill(void);
-    extern void NutThreadDestroy(void);
-    extern void NutThreadExit(void);
+extern void NutThreadKill(void);
+extern void NutThreadDestroy(void);
+extern void NutThreadExit(void);
 
-    extern void NutThreadResume(void);
-    extern void NutThreadWake(HANDLE timer, HANDLE th);
-    extern void NutThreadYield(void);
+extern void NutThreadResume(void);
+extern void NutThreadWake(HANDLE timer, HANDLE th);
+extern void NutThreadYield(void);
 
-    extern void NutThreadAddPriQueue(NUTTHREADINFO * td, NUTTHREADINFO ** tqpp);
-    extern void NutThreadRemoveQueue(NUTTHREADINFO * td, NUTTHREADINFO * volatile *tqpp);
+extern void NutThreadAddPriQueue(NUTTHREADINFO * td, NUTTHREADINFO * volatile *tqpp);
+extern void NutThreadRemoveQueue(NUTTHREADINFO * td, NUTTHREADINFO * volatile *tqpp);
 
-    extern void NutThreadSwitch(void);
-    extern HANDLE GetThreadByName(char * name);
+extern void NutThreadSwitch(void);
+extern HANDLE GetThreadByName(char *name);
 
 /*!
  * \brief Macro for thread entry definitions.
@@ -199,13 +204,13 @@ extern "C" {
 void threadfn(void *arg) __attribute__ ((noreturn)); \
 void threadfn(void *arg)
 
-    extern void DumpThreads(HANDLE handle);
-
-    extern void *NutStackAlloc(size_t size);
-    extern int NutStackFree(void *block);
-    extern void NutStackAdd(void *addr, size_t size);
-
-#ifdef __cplusplus
-}
+#if defined (NUTMEM_STACKHEAP)
+/* Stack resides in internal memory */
+extern void *NutStackAlloc(size_t size);
+extern int NutStackFree(void *block);
+extern void NutStackAdd(void *addr, size_t size);
 #endif
+
+__END_DECLS
+/* */
 #endif
