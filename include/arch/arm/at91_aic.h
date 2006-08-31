bf46714f33dd8f2eee130e91bbac6a6ee97e8dd9
haraldkipp <haraldkipp@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2006-08-31 19:08:14 +0000

    Defining register offsets simplifies assembly programming.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@1654 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- include/arch/arm/at91_aic.h
+++ include/arch/arm/at91_aic.h
@@ -40,6 +40,9 @@
  * \verbatim
  *
  * $Log$
+ * Revision 1.2  2006/08/31 19:08:14  haraldkipp
+ * Defining register offsets simplifies assembly programming.
+ *
  * Revision 1.1  2006/07/05 07:45:25  haraldkipp
  * Split on-chip interface definitions.
  *
@@ -92,68 +95,103 @@
 
 /*! \name Interrupt Vector Register */
 /*@{*/
-#define AIC_IVR     (AIC_BASE + 0x100)  /*!< \brief IRQ vector register address. */
+#define AIC_IVR_OFF                 0x00000100  /*!< \brief IRQ vector register offset. */
+#define AIC_IVR     (AIC_BASE + AIC_IVR_OFF)    /*!< \brief IRQ vector register address. */
 /*@}*/
 
 /*! \name Fast Interrupt Vector Register */
 /*@{*/
-#define AIC_FVR     (AIC_BASE + 0x104)  /*!< \brief FIQ vector register address. */
+#define AIC_FVR_OFF                 0x00000104  /*!< \brief FIQ vector register offset. */
+#define AIC_FVR     (AIC_BASE + AIC_FVR_OFF)    /*!< \brief FIQ vector register address. */
 /*@}*/
 
 /*! \name Interrupt Status Register */
 /*@{*/
-#define AIC_ISR     (AIC_BASE + 0x108)  /*!< \brief Interrupt status register address. */
-#define AIC_IRQID               0x0000001F      /*!< \brief Current interrupt identifier mask. */
+#define AIC_ISR_OFF                 0x00000108  /*!< \brief Interrupt status register offset. */
+#define AIC_ISR     (AIC_BASE + AIC_ISR_OFF)    /*!< \brief Interrupt status register address. */
+#define AIC_IRQID                   0x0000001F  /*!< \brief Current interrupt identifier mask. */
 /*@}*/
 
 /*! \name Interrupt Pending Register */
 /*@{*/
-#define AIC_IPR     (AIC_BASE + 0x10C)  /*!< \brief Interrupt pending register address. */
+#define AIC_IPR_OFF                 0x0000010C  /*!< \brief Interrupt pending register offset. */
+#define AIC_IPR     (AIC_BASE + AIC_IPR_OFF)    /*!< \brief Interrupt pending register address. */
 /*@}*/
 
 /*! \name Interrupt Mask Register */
 /*@{*/
-#define AIC_IMR     (AIC_BASE + 0x110)  /*!< \brief Interrupt mask register address. */
+#define AIC_IMR_OFF                 0x00000110  /*!< \brief Interrupt mask register offset. */
+#define AIC_IMR     (AIC_BASE + AIC_IMR_OFF)    /*!< \brief Interrupt mask register address. */
 /*@}*/
 
 /*! \name Interrupt Core Status Register */
 /*@{*/
-#define AIC_CISR    (AIC_BASE + 0x114)  /*!< \brief Core interrupt status register address. */
-#define AIC_NFIQ                0x00000001      /*!< \brief Core FIQ Status */
-#define AIC_NIRQ                0x00000002      /*!< \brief Core IRQ Status */
+#define AIC_CISR_OFF                0x00000114  /*!< \brief Core interrupt status register offset. */
+#define AIC_CISR    (AIC_BASE + AIC_CISR_OFF)   /*!< \brief Core interrupt status register address. */
+#define AIC_NFIQ                    0x00000001  /*!< \brief Core FIQ Status */
+#define AIC_NIRQ                    0x00000002  /*!< \brief Core IRQ Status */
 /*@}*/
 
 /*! \name Interrupt Enable Command Register */
 /*@{*/
-#define AIC_IECR    (AIC_BASE + 0x120)  /*!< \brief Interrupt enable command register address. */
+#define AIC_IECR_OFF                0x00000120  /*!< \brief Interrupt enable command register offset. */
+#define AIC_IECR    (AIC_BASE + AIC_IECR_OFF)   /*!< \brief Interrupt enable command register address. */
 /*@}*/
 
 /*! \name Interrupt Disable Command Register */
 /*@{*/
-#define AIC_IDCR    (AIC_BASE + 0x124)  /*!< \brief Interrupt disable command register address. */
+#define AIC_IDCR_OFF                0x00000124  /*!< \brief Interrupt disable command register offset. */
+#define AIC_IDCR    (AIC_BASE + AIC_IDCR_OFF)   /*!< \brief Interrupt disable command register address. */
 /*@}*/
 
 /*! \name Interrupt Clear Command Register */
 /*@{*/
-#define AIC_ICCR    (AIC_BASE + 0x128)  /*!< \brief Interrupt clear command register address. */
+#define AIC_ICCR_OFF                0x00000128  /*!< \brief Interrupt clear command register offset. */
+#define AIC_ICCR    (AIC_BASE + AIC_ICCR_OFF)   /*!< \brief Interrupt clear command register address. */
 /*@}*/
 
 /*! \name Interrupt Set Command Register */
 /*@{*/
-#define AIC_ISCR    (AIC_BASE + 0x12C)  /*!< \brief Interrupt set command register address. */
+#define AIC_ISCR_OFF                0x0000012C  /*!< \brief Interrupt set command register offset. */
+#define AIC_ISCR    (AIC_BASE + AIC_ISCR_OFF)   /*!< \brief Interrupt set command register address. */
 /*@}*/
 
 /*! \name End Of Interrupt Command Register */
 /*@{*/
-#define AIC_EOICR   (AIC_BASE + 0x130)  /*!< \brief End of interrupt command register address. */
+#define AIC_EOICR_OFF               0x00000130  /*!< \brief End of interrupt command register offset. */
+#define AIC_EOICR   (AIC_BASE + AIC_EOICR_OFF)  /*!< \brief End of interrupt command register address. */
 /*@}*/
 
 /*! \name Spurious Interrupt Vector Register */
 /*@{*/
-#define AIC_SPU     (AIC_BASE + 0x134)  /*!< \brief Spurious vector register address. */
+#define AIC_SPU_OFF                 0x00000134  /*!< \brief Spurious vector register offset. */
+#define AIC_SPU     (AIC_BASE + AIC_SPU_OFF)    /*!< \brief Spurious vector register address. */
+/*@}*/
+
+/*! \name Debug Control Register */
+/*@{*/
+#define AIC_DCR_OFF                 0x0000138   /*!< \brief Debug control register offset. */
+#define AIC_DCR     (AIC_BASE + AIC_DCR_OFF)    /*!< \brief Debug control register address. */
+/*@}*/
+
+/*! \name Fast Forcing Enable Register */
+/*@{*/
+#define AIC_FFER_OFF                0x00000140  /*!< \brief Fast forcing enable register offset. */
+#define AIC_FFER    (AIC_BASE + AIC_FFER_OFF)   /*!< \brief Fast forcing enable register address. */
+/*@}*/
+
+/*! \name Fast Forcing Disable Register */
+/*@{*/
+#define AIC_FFDR_OFF                0x00000144  /*!< \brief Fast forcing disable register address. */
+#define AIC_FFDR    (AIC_BASE + AIC_FFDR_OFF)   /*!< \brief Fast forcing disable register address. */
+/*@}*/
+
+/*! \name Fast Forcing Status Register */
+/*@{*/
+#define AIC_FFSR_OFF                0x00000148  /*!< \brief Fast forcing status register address. */
+#define AIC_FFSR    (AIC_BASE + AIC_FFSR_OFF)   /*!< \brief Fast forcing status register address. */
 /*@}*/
 
 /*@} xgNutArchArmAt91Aic */
 
 #endif                          /* _ARCH_ARM_AT91_AIC_H_ */
-
--- include/arch/arm/at91_pmc.h
+++ include/arch/arm/at91_pmc.h
@@ -40,6 +40,9 @@
  * \verbatim
  *
  * $Log$
+ * Revision 1.5  2006/08/31 19:08:14  haraldkipp
+ * Defining register offsets simplifies assembly programming.
+ *
  * Revision 1.4  2006/08/05 11:59:23  haraldkipp
  * Wrong PMC register offsets fixed.
  *
@@ -61,44 +64,70 @@
  */
 /*@{*/
 
+/*! \name System Clock Enable, Disable and Status Register */
+/*@{*/
 #define PMC_SCER_OFF                0x00000000  /*!< \brief System clock enable register offset. */
-#define PMC_SCER    (PMC_BASE + PMC_SCER_OFF)   /*!< \brief System clock enable register. */
+#define PMC_SCER    (PMC_BASE + PMC_SCER_OFF)   /*!< \brief System clock enable register address. */
 #define PMC_SCDR_OFF                0x00000004  /*!< \brief System clock disable register offset. */
-#define PMC_SCDR    (PMC_BASE + PMC_SCDR_OFF)   /*!< \brief System clock disable register. */
+#define PMC_SCDR    (PMC_BASE + PMC_SCDR_OFF)   /*!< \brief System clock disable register address. */
 #define PMC_SCSR_OFF                0x00000008  /*!< \brief System clock status register offset. */
-#define PMC_SCSR    (PMC_BASE + PMC_SCSR_OFF)   /*!< \brief System clock status register. */
+#define PMC_SCSR    (PMC_BASE + PMC_SCSR_OFF)   /*!< \brief System clock status register address. */
+
 #define PMC_PCK                     0x00000001  /*!< \brief Processor clock. */
+#define PMC_UHP                     0x00000040  /*!< \brief USB host port clock. */
 #define PMC_UDP                     0x00000080  /*!< \brief USB device port clock. */
 #define PMC_PCK0                    0x00000100  /*!< \brief Programmable clock 0 output. */
 #define PMC_PCK1                    0x00000200  /*!< \brief Programmable clock 1 output. */
 #define PMC_PCK2                    0x00000400  /*!< \brief Programmable clock 2 output. */
 #define PMC_PCK3                    0x00000800  /*!< \brief Programmable clock 3 output. */
+/*@}*/
 
+/*! \name Peripheral Clock Enable, Disable and Status Register */
+/*@{*/
 #define PMC_PCER_OFF                0x00000010  /*!< \brief Peripheral clock enable register offset. */
-#define PMC_PCER    (PMC_BASE + PMC_PCER_OFF)   /*!< \brief Peripheral clock enable register. */
+#define PMC_PCER    (PMC_BASE + PMC_PCER_OFF)   /*!< \brief Peripheral clock enable register address. */
 #define PMC_PCDR_OFF                0x00000014  /*!< \brief Peripheral clock disable register offset. */
-#define PMC_PCDR    (PMC_BASE + PMC_PCDR_OFF)   /*!< \brief Peripheral clock disable register. */
+#define PMC_PCDR    (PMC_BASE + PMC_PCDR_OFF)   /*!< \brief Peripheral clock disable register address. */
 #define PMC_PCSR_OFF                0x00000018  /*!< \brief Peripheral clock status register offset. */
-#define PMC_PCSR    (PMC_BASE + PMC_PCSR_OFF)   /*!< \brief Peripheral clock status register. */
+#define PMC_PCSR    (PMC_BASE + PMC_PCSR_OFF)   /*!< \brief Peripheral clock status register address. */
+/*@}*/
 
-#define CKGR_MOR_OFF                0x00000020  /*!< \brief Clock generator main oscillator register offset. */
-#define CKGR_MOR    (PMC_BASE + CKGR_MOR_OFF)   /*!< \brief Clock generator main oscillator register. */
+/*! \name Clock Generator Main Oscillator Register */
+/*@{*/
+#define CKGR_MOR_OFF                0x00000020  /*!< \brief Main oscillator register offset. */
+#define CKGR_MOR    (PMC_BASE + CKGR_MOR_OFF)   /*!< \brief Main oscillator register address. */
+
 #define CKGR_MOSCEN                 0x00000001  /*!< \brief Main oscillator enable. */
 #define CKGR_OSCBYPASS              0x00000002  /*!< \brief Main oscillator bypass. */
-#define CKGR_OSCOUNT                0x0000FF00  /*!< \brief Main oscillator start-up time. */
+#define CKGR_OSCOUNT                0x0000FF00  /*!< \brief Main oscillator start-up time mask. */
+#define CKGR_OSCOUNT_LSB                    8   /*!< \brief Main oscillator start-up time LSB. */
+/*@}*/
 
-#define CKGR_MCFR_OFF               0x00000024  /*!< \brief Clock generator main clock frequency register offset. */
-#define CKGR_MCFR   (PMC_BASE + CKGR_MCFR_OFF)  /*!< \brief Clock generator main clock frequency register. */
-#define CKGR_MAINF                  0x0000FFFF  /*!< \brief Main clock frequency. */
+/*! \name Clock Generator Main Clock Frequency Register */
+/*@{*/
+#define CKGR_MCFR_OFF               0x00000024  /*!< \brief Main clock frequency register offset. */
+#define CKGR_MCFR   (PMC_BASE + CKGR_MCFR_OFF)  /*!< \brief Main clock frequency register address. */
+
+#define CKGR_MAINF                  0x0000FFFF  /*!< \brief Main clock frequency mask mask. */
+#define CKGR_MAINF_OFF                      0   /*!< \brief Main clock frequency mask LSB. */
 #define CKGR_MAINRDY                0x00010000  /*!< \brief Main clock ready. */
+/*@}*/
 
+/*! \name PLL Registers */
+/*@{*/
+#define CKGR_PLLAR_OFF              0x00000028  /*!< \brief Clock generator PLL register offset. */
+#define CKGR_PLLAR  (PMC_BASE + CKGR_PLLAR_OFF) /*!< \brief Clock generator PLL register address. */
+#define CKGR_PLLBR_OFF              0x0000002C  /*!< \brief Clock generator PLL register offset. */
+#define CKGR_PLLBR  (PMC_BASE + CKGR_PLLBR_OFF) /*!< \brief Clock generator PLL register address. */
 #define CKGR_PLLR_OFF               0x0000002C  /*!< \brief Clock generator PLL register offset. */
-#define CKGR_PLLR   (PMC_BASE + CKGR_PLLR_OFF)  /*!< \brief Clock generator PLL register. */
+#define CKGR_PLLR   (PMC_BASE + CKGR_PLLR_OFF)  /*!< \brief Clock generator PLL register address. */
+
 #define CKGR_DIV                    0x000000FF  /*!< \brief Divider. */
 #define CKGR_DIV_LSB                        0   /*!< \brief Least significant bit of the divider. */
 #define CKGR_DIV_0                  0x00000000  /*!< \brief Divider output is 0. */
 #define CKGR_DIV_BYPASS             0x00000001  /*!< \brief Divider is bypassed. */
-#define CKGR_PLLCOUNT               0x00003F00  /*!< \brief PLL counter. */
+#define CKGR_PLLCOUNT               0x00003F00  /*!< \brief PLL counter mask. */
+#define CKGR_PLLCOUNT_LSB                   8   /*!< \brief PLL counter LSB. */
 #define CKGR_OUT                    0x0000C000  /*!< \brief PLL output frequency range. */
 #define CKGR_OUT_0                  0x00000000  /*!< \brief Please refer to the PLL datasheet. */
 #define CKGR_OUT_1                  0x00004000  /*!< \brief Please refer to the PLL datasheet. */
@@ -110,26 +139,31 @@
 #define CKGR_USBDIV_0               0x00000000  /*!< \brief Divider output is PLL clock output. */
 #define CKGR_USBDIV_1               0x10000000  /*!< \brief Divider output is PLL clock output divided by 2. */
 #define CKGR_USBDIV_2               0x20000000  /*!< \brief Divider output is PLL clock output divided by 4. */
+/*@}*/
 
+/*! \name Master Clock Register */
+/*@{*/
 #define PMC_MCKR_OFF                0x00000030  /*!< \brief Master clock register offset. */
-#define PMC_MCKR    (PMC_BASE + PMC_MCKR_OFF)   /*!< \brief Master clock register. */
+#define PMC_MCKR    (PMC_BASE + PMC_MCKR_OFF)   /*!< \brief Master clock register address. */
 #define PMC_ACKR_OFF                0x00000034  /*!< \brief Application clock register offset. */
-#define PMC_ACKR    (PMC_BASE + PMC_ACKR_OFF)   /*!< \brief Application clock register. */
+#define PMC_ACKR    (PMC_BASE + PMC_ACKR_OFF)   /*!< \brief Application clock register address. */
 #define PMC_PCKR0_OFF               0x00000040  /*!< \brief Programmable clock 0 register offset. */
-#define PMC_PCKR0   (PMC_BASE + PMC_PCKR0_OFF)  /*!< \brief Programmable clock 0 register. */
+#define PMC_PCKR0   (PMC_BASE + PMC_PCKR0_OFF)  /*!< \brief Programmable clock 0 register address. */
 #define PMC_PCKR1_OFF               0x00000044  /*!< \brief Programmable clock 1 register offset. */
-#define PMC_PCKR1   (PMC_BASE + PMC_PCKR1_OFF)  /*!< \brief Programmable clock 1 register. */
+#define PMC_PCKR1   (PMC_BASE + PMC_PCKR1_OFF)  /*!< \brief Programmable clock 1 register address. */
 #define PMC_PCKR2_OFF               0x00000048  /*!< \brief Programmable clock 2 register offset. */
-#define PMC_PCKR2   (PMC_BASE + PMC_PCKR2_OFF)  /*!< \brief Programmable clock 2 register. */
+#define PMC_PCKR2   (PMC_BASE + PMC_PCKR2_OFF)  /*!< \brief Programmable clock 2 register address. */
 #define PMC_PCKR3_OFF               0x0000004C  /*!< \brief Programmable clock 3 register offset. */
-#define PMC_PCKR3   (PMC_BASE + PMC_PCKR3_OFF)  /*!< \brief Programmable clock 3 register. */
+#define PMC_PCKR3   (PMC_BASE + PMC_PCKR3_OFF)  /*!< \brief Programmable clock 3 register address. */
+
 #define PMC_CSS                     0x00000003  /*!< \brief Clock selection mask. */
-#define PMC_CSS_LSB                         0   /*!< \brief Least significant bit of the clock selection. */
 #define PMC_CSS_SLOW_CLK            0x00000000  /*!< \brief Slow clock selected. */
 #define PMC_CSS_MAIN_CLK            0x00000001  /*!< \brief Main clock selected. */
+#define PMC_CSS_PLLA_CLK            0x00000002  /*!< \brief PLL A clock selected. */
+#define PMC_CSS_PLLB_CLK            0x00000003  /*!< \brief PLL B clock selected. */
 #define PMC_CSS_PLL_CLK             0x00000003  /*!< \brief PLL clock selected. */
 #define PMC_PRES                    0x0000001C  /*!< \brief Clock prescaler mask. */
-#define PMC_PRES_LSB                        2   /*!< \brief Clock prescaler's least signigifcant bit. */
+#define PMC_PRES_LSB                        2   /*!< \brief Clock prescaler LSB. */
 #define PMC_PRES_CLK                0x00000000  /*!< \brief Selected clock, not divided. */
 #define PMC_PRES_CLK_2              0x00000004  /*!< \brief Selected clock divided by 2. */
 #define PMC_PRES_CLK_4              0x00000008  /*!< \brief Selected clock divided by 4. */
@@ -137,22 +171,34 @@
 #define PMC_PRES_CLK_16             0x00000010  /*!< \brief Selected clock divided by 16. */
 #define PMC_PRES_CLK_32             0x00000014  /*!< \brief Selected clock divided by 32. */
 #define PMC_PRES_CLK_64             0x00000018  /*!< \brief Selected clock divided by 64. */
+#define PMC_MDIV                    0x00000300  /*!< \brief Master clock division mask. */
+#define PMC_MDIV_1                  0x00000000  /*!< \brief Processor clock, not divided. */
+#define PMC_MDIV_2                  0x00000100  /*!< \brief Processor clock divided by 2. */
+#define PMC_MDIV_4                  0x00000200  /*!< \brief Processor clock divided by 4. */
+/*@}*/
 
-#define PMC_IER_OFF                 0x00000060  /*!< \brief PMC interrupt enable register offset. */
-#define PMC_IER     (PMC_BASE + PMC_IER_OFF)    /*!< \brief PMC interrupt enable register. */
-#define PMC_IDR_OFF                 0x00000064  /*!< \brief PMC interrupt disable register offset. */
-#define PMC_IDR     (PMC_BASE + PMC_IDR_OFF)    /*!< \brief PMC interrupt disable register. */
-#define PMC_SR_OFF                  0x00000068  /*!< \brief PMC status register offset. */
-#define PMC_SR      (PMC_BASE + PMC_SR_OFF)     /*!< \brief PMC status register. */
-#define PMC_IMR_OFF                 0x0000006C  /*!< \brief PMC interrupt mask register offset. */
-#define PMC_IMR     (PMC_BASE + PMC_IMR_OFF)    /*!< \brief PMC interrupt mask register. */
+/*! \name Power Management Status and Interrupt Registers */
+/*@{*/
+#define PMC_IER_OFF                 0x00000060  /*!< \brief Interrupt enable register offset. */
+#define PMC_IER     (PMC_BASE + PMC_IER_OFF)    /*!< \brief Interrupt enable register address. */
+#define PMC_IDR_OFF                 0x00000064  /*!< \brief Interrupt disable register offset. */
+#define PMC_IDR     (PMC_BASE + PMC_IDR_OFF)    /*!< \brief Interrupt disable register address. */
+#define PMC_SR_OFF                  0x00000068  /*!< \brief Status register offset. */
+#define PMC_SR      (PMC_BASE + PMC_SR_OFF)     /*!< \brief Status register address. */
+#define PMC_IMR_OFF                 0x0000006C  /*!< \brief Interrupt mask register offset. */
+#define PMC_IMR     (PMC_BASE + PMC_IMR_OFF)    /*!< \brief Interrupt mask register address. */
+
 #define PMC_MOSCS                   0x00000001  /*!< \brief Main oscillator. */
+#define PMC_LOCKA                   0x00000002  /*!< \brief PLL A lock. */
+#define PMC_LOCKB                   0x00000004  /*!< \brief PLL B lock. */
 #define PMC_LOCK                    0x00000004  /*!< \brief PLL lock. */
 #define PMC_MCKRDY                  0x00000008  /*!< \brief Master clock ready. */
+#define PMC_OSC_SEL                 0x00000080  /*!< \brief Slow clock oscillator selection. */
 #define PMC_PCKRDY0                 0x00000100  /*!< \brief Programmable clock 0 ready. */
 #define PMC_PCKRDY1                 0x00000200  /*!< \brief Programmable clock 1 ready. */
 #define PMC_PCKRDY2                 0x00000400  /*!< \brief Programmable clock 2 ready. */
 #define PMC_PCKRDY3                 0x00000800  /*!< \brief Programmable clock 3 ready. */
+/*@}*/
 
 /*@} xgNutArchArmAt91Pmc */
 
