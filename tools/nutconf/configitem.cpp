b49e5e161b5b2d5daa5a532d11436364a653ee81
olereinhardt <olereinhardt@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2008-08-01 19:41:18 +0000

    Added cast to char* in line 698 to avoid compiling bug on linux
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@2228 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- tools/nutconf/configitem.cpp
+++ tools/nutconf/configitem.cpp
@@ -20,6 +20,9 @@
 
 /*
  * $Log: configitem.cpp,v $
+ * Revision 1.11  2008/08/01 19:41:18  olereinhardt
+ * Added cast to char* in line 698 to avoid compiling bug on linux
+ *
  * Revision 1.10  2008/07/29 07:29:36  haraldkipp
  * Honor editing of boolean settings.
  *
@@ -695,7 +698,7 @@ void CConfigItem::SetActive(bool ena)
     if (m_option) {
         m_option->nco_active = ena;
         if (ena && m_option->nco_value == NULL) {
-            m_option->nco_value = strdup(wxT(""));
+            m_option->nco_value = strdup((char*)wxT(""));
         }
     }
 }
