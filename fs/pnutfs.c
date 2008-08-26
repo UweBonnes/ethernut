b95e9202c9bacc0913d5aed2d60bcf146516d988
thornen <thornen@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2008-08-26 11:20:51 +0000

    Added support for MMnet03..04 and MMnet102..104 display New BSD formula applied
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@2266 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- fs/pnutfs.c
+++ fs/pnutfs.c
@@ -1,34 +1,40 @@
 /*
- * Copyright (C) 2004-2006 by egnite Software GmbH. All rights reserved.
+ * Copyright (C) 2001-2006 by egnite Software GmbH. All rights reserved.
+ * Copyright (C)  2008 by Propox sp. z o.o. 
+ *                          office@propox.com
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions 
  * are met:
- *
+ * 
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
+ *     notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
  * 3. Neither the name of the copyright holders nor the names of
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY EGNITE SOFTWARE GMBH AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL EGNITE
- * SOFTWARE GMBH OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
- * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
  *
  * For additional information see http://www.ethernut.de/
+ *
  */
+
+
 
 /*!
  * \file fs/pnutfs.c
@@ -37,8 +43,12 @@
  * \verbatim
  *
  * $Log$
- * Revision 1.11  2008/08/11 06:59:42  haraldkipp
- * BSD types replaced by stdint types (feature request #1282721).
+ * Revision 1.12  2008/08/26 11:19:02  thornen
+ * Added support for MMnet03..04 and MMnet102..104 display
+ * New BSD formula applied
+ *
+ * Revision 1.11  2008/08/26 09:06:17  michalolejniczak
+ * Added support for MMnet03..04 and MMnet102..104
  *
  * Revision 1.10  2006/08/01 07:43:48  haraldkipp
  * PNUT file system failed after some modifications done previously for the
@@ -145,7 +155,8 @@
 #ifdef ARTHERNET1
 /* Default for Arthernet 1. */
 #define PNUTBANK_COUNT 15
-#elif MMNET02
+#elif MMNET02  || MMNET03  || MMNET04 ||\
+	  MMNET102 || MMNET103 || MMNET104  
 /* Default for MMnte02. */
 #define PNUTBANK_COUNT 6
 #else
@@ -188,8 +199,8 @@ typedef short PNUT_BLKNUM;
  * \note The node structure must fit into a block.
  */
 typedef struct {
-    uint8_t node_type;           /*!< Type of this node, 0=file, 1=dir */
-    uint8_t node_refs;           /*!< Reference counter: Number of active open calls. */
+    u_char node_type;           /*!< Type of this node, 0=file, 1=dir */
+    u_char node_refs;           /*!< Reference counter: Number of active open calls. */
     /*!
      * \brief Number of links.
      *
@@ -200,8 +211,8 @@ typedef struct {
      * For directory nodes the number represents the number
      * of subdirectories plus 2.
      */
-    uint16_t node_links;
-    uint32_t node_size;           /*!< Total size of the data area. */
+    u_short node_links;
+    u_long node_size;           /*!< Total size of the data area. */
     time_t node_mtime;          /*!< Time of last modification. */
     PNUT_BLKNUM node_blocks[PNUT_BLOCKS_PER_NODE];      /*!< Allocated data blocks. */
 } PNUT_NODE;
@@ -212,7 +223,7 @@ typedef struct {
  * This is a calculated value and depends on the definition of the
  * block size and the size of a directory entry.
  */
-#define PNUT_MAX_NAMELEN    (PNUT_DIRENT_SIZE - sizeof(PNUT_BLKNUM) - sizeof(uint8_t) - 1)
+#define PNUT_MAX_NAMELEN    (PNUT_DIRENT_SIZE - sizeof(PNUT_BLKNUM) - sizeof(u_char) - 1)
 
 /*!
  * \brief Directory entry structure.
@@ -234,7 +245,7 @@ typedef struct {
      *
      * Set to one if this entry is in use.
      */
-    uint8_t dir_inuse;
+    u_char dir_inuse;
     /*!
      * \brief Name of this entry.
      *
@@ -283,7 +294,7 @@ typedef struct _PNUTFILE PNUTFILE;
  */
 struct _PNUTFILE {
     PNUT_BLKNUM f_node;         /* Node of the file or directory. */
-    uint32_t f_pos;               /* Current file pointer position. */
+    u_long f_pos;               /* Current file pointer position. */
     u_int f_flag;               /* File mode. */
 };
 
@@ -421,7 +432,7 @@ static void PnutBlockRelease(PNUT_BLKNUM blk)
  *
  * \return Block index of this node or -1 if there are no more blocks.
  */
-static PNUT_BLKNUM PnutNodeAlloc(uint8_t type)
+static PNUT_BLKNUM PnutNodeAlloc(u_char type)
 {
     PNUT_BLKNUM node = PnutBlockAlloc();
 
@@ -483,7 +494,7 @@ static void PnutNodeRelease(PNUT_BLKNUM node)
  *
  * \return 0 on success, otherwise returns an error code.
  */
-static int PnutNodeGetDataPtr(PNUT_BLKNUM node, uint32_t pos, void **buffer, size_t * size, int create)
+static int PnutNodeGetDataPtr(PNUT_BLKNUM node, u_long pos, void **buffer, size_t * size, int create)
 {
     int blkidx;                 /* Number of full blocks */
     int rc = EINVAL;
@@ -529,7 +540,7 @@ static int PnutNodeGetDataPtr(PNUT_BLKNUM node, uint32_t pos, void **buffer, siz
 static int PnutDirIsEmpty(PNUT_BLKNUM node)
 {
     int rc = 1;
-    uint32_t pos;
+    u_long pos;
     size_t size;
     PNUT_DIRENTRY *entry;
 
@@ -569,7 +580,7 @@ static int PnutDirIsEmpty(PNUT_BLKNUM node)
 static int PnutDirFindEntry(PNUT_BLKNUM node, CONST char *path, size_t len, PNUT_DIRENTRY ** entry)
 {
     int rc = ENOENT;
-    uint32_t pos;
+    u_long pos;
     size_t size;
 
     /* Loop through the data blocks of this directory node. */
@@ -750,7 +761,7 @@ static int PnutDirClose(DIR * dir)
 static int PnutDirRead(DIR * dir)
 {
     int rc = -1;
-    uint32_t pos;
+    u_long pos;
     PNUT_DIRENTRY *entry;
     size_t size;
     PNUTFILE *fp = dir->dd_fd->nf_fcb;
@@ -771,7 +782,7 @@ static int PnutDirRead(DIR * dir)
         if (size >= PNUT_DIRENT_SIZE && entry->dir_inuse) {
             memset(ent, 0, sizeof(struct dirent));
             ent->d_fileno = entry->dir_node;
-            ent->d_namlen = (uint8_t) strlen(entry->dir_name);
+            ent->d_namlen = (u_char) strlen(entry->dir_name);
             strcpy(ent->d_name, entry->dir_name);
             break;
         }
@@ -791,7 +802,7 @@ static int PnutDirRead(DIR * dir)
 static int PnutDirAddEntry(PNUT_BLKNUM dnode, CONST char *name, PNUT_BLKNUM enode)
 {
     int rc = 0;
-    uint32_t pos = 0;
+    u_long pos = 0;
     size_t size;
     PNUT_DIRENTRY *entry;
     PNUT_NODE *np;
@@ -1144,7 +1155,7 @@ static int PnutFileWrite(NUTFILE * nfp, CONST void *buffer, int len)
     int ec = 0;
     int rc = 0;
     PNUT_BLKNUM node = fp->f_node;
-    uint8_t *blkptr;
+    u_char *blkptr;
     size_t blksiz;
     CONST char *buf = buffer;
 
@@ -1196,7 +1207,7 @@ static int PnutFileRead(NUTFILE * nfp, void *buffer, int len)
     int ec = 0;
     int rc = 0;
     PNUT_BLKNUM node = fp->f_node;
-    uint8_t *blkptr;
+    u_char *blkptr;
     size_t blksiz;
     char *buf = buffer;
 
