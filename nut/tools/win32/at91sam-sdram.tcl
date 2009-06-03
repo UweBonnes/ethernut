0420ad8bfb32c3d6017961f179299d96780208ca
haraldkipp <haraldkipp@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2009-06-03 09:19:56 +0000

    More targets supported with 'make burn' and SAM-BA.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@2619 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- nut/tools/win32/at91sam-sdram.tcl
+++ nut/tools/win32/at91sam-sdram.tcl
@@ -1,8 +1,11 @@
 global target
 
-SDRAM::initSDRAM_133
-send_file {SDRAM} [lindex $argv 3] 0x20000000 0
-compare_file  {SDRAM} [lindex $argv 3] 0x20000000 0
-go 0x20000000
-puts [lindex $argv 3]
+# Init SDRAM
+#SDRAM::initSDRAM_133
+GENERIC::Init $RAM::appletAddr $RAM::appletFileName [list $::target(comType) $GENERIC::traceLevel $BOARD::vddmem $BOARD::ramType]
 
+send_file {SDRAM} [lindex $argv 4] [lindex $argv 3] 0
+compare_file {SDRAM} [lindex $argv 4] [lindex $argv 3] 0
+
+# Start the uploaded image
+go [lindex $argv 3]
