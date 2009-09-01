20ae1f93e41ee3c57ccebf88c4a37647e4dc17ae
haraldkipp <haraldkipp@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2009-09-01 18:45:12 +0000

    Added missing default configuration file for PCA9555.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@2662 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

---
+++ nut/include/cfg/pca9555.h
@@ -0,0 +1,17 @@
+#ifndef _CFG_PCA9555_H_
+#define _CFG_PCA9555_H_
+
+/*!
+ * \file cfg/pca9555.h
+ *
+ * When using the Configurator, a file with the same name may be created in the
+ * build tree, which replaces this file.
+ *
+ * \verbatim
+ *
+ * $Id$
+ *
+ * \endverbatim
+ */
+
+#endif
