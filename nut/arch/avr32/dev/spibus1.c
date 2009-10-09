5282ee5851b043b1033e9027781d6fe84b79357b
haraldkipp <haraldkipp@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2009-10-09 14:59:42 +0000

    Indention should follow Nut/OS coding style.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@2743 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- nut/arch/avr32/dev/spibus1.c
+++ nut/arch/avr32/dev/spibus1.c
@@ -65,9 +65,19 @@
 #undef GPIO_ID
 #define GPIO_ID SPI1_CS0_PIO_ID
 #include <cfg/arch/porttran.h>
-static INLINE void SPI1_CS0_LO(void) { GPIO_SET_LO(SPI1_CS0_PIO_BIT); }
-static INLINE void SPI1_CS0_HI(void) { GPIO_SET_HI(SPI1_CS0_PIO_BIT); }
-static INLINE void SPI1_CS0_SO(void) { GPIO_ENABLE(SPI1_CS0_PIO_BIT); GPIO_OUTPUT(SPI1_CS0_PIO_BIT); }
+static INLINE void SPI1_CS0_LO(void)
+{
+    GPIO_SET_LO(SPI1_CS0_PIO_BIT);
+}
+static INLINE void SPI1_CS0_HI(void)
+{
+    GPIO_SET_HI(SPI1_CS0_PIO_BIT);
+}
+static INLINE void SPI1_CS0_SO(void)
+{
+    GPIO_ENABLE(SPI1_CS0_PIO_BIT);
+    GPIO_OUTPUT(SPI1_CS0_PIO_BIT);
+}
 #else
 #define SPI1_CS0_LO()
 #define SPI1_CS0_HI()
@@ -80,9 +90,19 @@ static INLINE void SPI1_CS0_SO(void) { GPIO_ENABLE(SPI1_CS0_PIO_BIT); GPIO_OUTPU
 #undef GPIO_ID
 #define GPIO_ID SPI1_CS1_PIO_ID
 #include <cfg/arch/porttran.h>
-static INLINE void SPI1_CS1_LO(void) { GPIO_SET_LO(SPI1_CS1_PIO_BIT); }
-static INLINE void SPI1_CS1_HI(void) { GPIO_SET_HI(SPI1_CS1_PIO_BIT); }
-static INLINE void SPI1_CS1_SO(void) { GPIO_ENABLE(SPI1_CS1_PIO_BIT); GPIO_OUTPUT(SPI1_CS1_PIO_BIT); }
+static INLINE void SPI1_CS1_LO(void)
+{
+    GPIO_SET_LO(SPI1_CS1_PIO_BIT);
+}
+static INLINE void SPI1_CS1_HI(void)
+{
+    GPIO_SET_HI(SPI1_CS1_PIO_BIT);
+}
+static INLINE void SPI1_CS1_SO(void)
+{
+    GPIO_ENABLE(SPI1_CS1_PIO_BIT);
+    GPIO_OUTPUT(SPI1_CS1_PIO_BIT);
+}
 #else
 #define SPI1_CS1_LO()
 #define SPI1_CS1_HI()
@@ -95,9 +115,19 @@ static INLINE void SPI1_CS1_SO(void) { GPIO_ENABLE(SPI1_CS1_PIO_BIT); GPIO_OUTPU
 #undef GPIO_ID
 #define GPIO_ID SPI1_CS2_PIO_ID
 #include <cfg/arch/porttran.h>
-static INLINE void SPI1_CS2_LO(void) { GPIO_SET_LO(SPI1_CS2_PIO_BIT); }
-static INLINE void SPI1_CS2_HI(void) { GPIO_SET_HI(SPI1_CS2_PIO_BIT); }
-static INLINE void SPI1_CS2_SO(void) { GPIO_ENABLE(SPI1_CS2_PIO_BIT); GPIO_OUTPUT(SPI1_CS2_PIO_BIT); }
+static INLINE void SPI1_CS2_LO(void)
+{
+    GPIO_SET_LO(SPI1_CS2_PIO_BIT);
+}
+static INLINE void SPI1_CS2_HI(void)
+{
+    GPIO_SET_HI(SPI1_CS2_PIO_BIT);
+}
+static INLINE void SPI1_CS2_SO(void)
+{
+    GPIO_ENABLE(SPI1_CS2_PIO_BIT);
+    GPIO_OUTPUT(SPI1_CS2_PIO_BIT);
+}
 #else
 #define SPI1_CS2_LO()
 #define SPI1_CS2_HI()
@@ -110,9 +140,19 @@ static INLINE void SPI1_CS2_SO(void) { GPIO_ENABLE(SPI1_CS2_PIO_BIT); GPIO_OUTPU
 #undef GPIO_ID
 #define GPIO_ID SPI1_CS3_PIO_ID
 #include <cfg/arch/porttran.h>
-static INLINE void SPI1_CS3_LO(void) { GPIO_SET_LO(SPI1_CS3_PIO_BIT); }
-static INLINE void SPI1_CS3_HI(void) { GPIO_SET_HI(SPI1_CS3_PIO_BIT); }
-static INLINE void SPI1_CS3_SO(void) { GPIO_ENABLE(SPI1_CS3_PIO_BIT); GPIO_OUTPUT(SPI1_CS3_PIO_BIT); }
+static INLINE void SPI1_CS3_LO(void)
+{
+    GPIO_SET_LO(SPI1_CS3_PIO_BIT);
+}
+static INLINE void SPI1_CS3_HI(void)
+{
+    GPIO_SET_HI(SPI1_CS3_PIO_BIT);
+}
+static INLINE void SPI1_CS3_SO(void)
+{
+    GPIO_ENABLE(SPI1_CS3_PIO_BIT);
+    GPIO_OUTPUT(SPI1_CS3_PIO_BIT);
+}
 #else
 #define SPI1_CS3_LO()
 #define SPI1_CS3_HI()
@@ -125,55 +165,55 @@ static INLINE void SPI1_CS3_SO(void) { GPIO_ENABLE(SPI1_CS3_PIO_BIT); GPIO_OUTPU
 */
 int Avr32Spi1ChipSelect(uint_fast8_t cs, uint_fast8_t hi)
 {
-	int rc = 0;
+    int rc = 0;
 
-	switch (cs) {
+    switch (cs) {
 #if defined(SPI1_CS0_PIO_BIT)
-	case 0:
-		if (hi) {
-			SPI1_CS0_HI();
-		} else {
-			SPI1_CS0_LO();
-		}
-		SPI1_CS0_SO();
-		break;
+    case 0:
+        if (hi) {
+            SPI1_CS0_HI();
+        } else {
+            SPI1_CS0_LO();
+        }
+        SPI1_CS0_SO();
+        break;
 #endif
 #if defined(SPI1_CS1_PIO_BIT)
-	case 1:
-		if (hi) {
-			SPI1_CS1_HI();
-		} else {
-			SPI1_CS1_LO();
-		}
-		SPI1_CS1_SO();
-		break;
+    case 1:
+        if (hi) {
+            SPI1_CS1_HI();
+        } else {
+            SPI1_CS1_LO();
+        }
+        SPI1_CS1_SO();
+        break;
 #endif
 #if defined(SPI1_CS2_PIO_BIT)
-	case 2:
-		if (hi) {
-			SPI1_CS2_HI();
-		} else {
-			SPI1_CS2_LO();
-		}
-		SPI1_CS2_SO();
-		break;
+    case 2:
+        if (hi) {
+            SPI1_CS2_HI();
+        } else {
+            SPI1_CS2_LO();
+        }
+        SPI1_CS2_SO();
+        break;
 #endif
 #if defined(SPI1_CS3_PIO_BIT)
-	case 3:
-		if (hi) {
-			SPI1_CS3_HI();
-		} else {
-			SPI1_CS3_LO();
-		}
-		SPI1_CS3_SO();
-		break;
+    case 3:
+        if (hi) {
+            SPI1_CS3_HI();
+        } else {
+            SPI1_CS3_LO();
+        }
+        SPI1_CS3_SO();
+        break;
 #endif
-	default:
-		errno = EIO;
-		rc = -1;
-		break;
-	}
-	return rc;
+    default:
+        errno = EIO;
+        rc = -1;
+        break;
+    }
+    return rc;
 }
 
 /*! \brief Select a device on the first SPI bus.
@@ -189,47 +229,51 @@ int Avr32Spi1ChipSelect(uint_fast8_t cs, uint_fast8_t hi)
 */
 int Avr32SpiBus1Select(NUTSPINODE * node, uint32_t tmo)
 {
-	int rc;
+    int rc;
 
-	/* Sanity check. */
-	NUTASSERT(node != NULL);
-	NUTASSERT(node->node_bus != NULL);
-	NUTASSERT(node->node_stat != NULL);
+    /* Sanity check. */
+    NUTASSERT(node != NULL);
+    NUTASSERT(node->node_bus != NULL);
+    NUTASSERT(node->node_stat != NULL);
 
-	/* Allocate the bus. */
-	rc = NutEventWait(&node->node_bus->bus_mutex, tmo);
-	if (rc) {
-		errno = EIO;
-	} else {
-		AVR32SPIREG *spireg = node->node_stat;
+    /* Allocate the bus. */
+    rc = NutEventWait(&node->node_bus->bus_mutex, tmo);
+    if (rc) {
+        errno = EIO;
+    } else {
+        AVR32SPIREG *spireg = node->node_stat;
 
-		/* Enable SPI peripherals and clock. */
-		GpioPinConfigSet( AVR32_GPIO_BANK(AVR32_SPI1_SCK_0_PIN), AVR32_GPIO_PIN(AVR32_SPI1_SCK_0_PIN), AVR32_GPIO_FUNCTION(AVR32_SPI1_SCK_0_FUNCTION) );
-		GpioPinConfigSet( AVR32_GPIO_BANK(AVR32_SPI1_MISO_0_PIN), AVR32_GPIO_PIN(AVR32_SPI1_MISO_0_PIN), AVR32_GPIO_FUNCTION(AVR32_SPI1_MISO_0_FUNCTION) );
-		GpioPinConfigSet( AVR32_GPIO_BANK(AVR32_SPI1_MOSI_0_PIN), AVR32_GPIO_PIN(AVR32_SPI1_MOSI_0_PIN), AVR32_GPIO_FUNCTION(AVR32_SPI1_MOSI_0_FUNCTION) );
-		GpioPinConfigSet( AVR32_GPIO_BANK(AVR32_SPI1_NPCS_0_PIN), AVR32_GPIO_PIN(AVR32_SPI1_NPCS_0_PIN), AVR32_GPIO_FUNCTION(AVR32_SPI1_NPCS_0_FUNCTION) );
+        /* Enable SPI peripherals and clock. */
+        GpioPinConfigSet(AVR32_GPIO_BANK(AVR32_SPI1_SCK_0_PIN), AVR32_GPIO_PIN(AVR32_SPI1_SCK_0_PIN),
+                         AVR32_GPIO_FUNCTION(AVR32_SPI1_SCK_0_FUNCTION));
+        GpioPinConfigSet(AVR32_GPIO_BANK(AVR32_SPI1_MISO_0_PIN), AVR32_GPIO_PIN(AVR32_SPI1_MISO_0_PIN),
+                         AVR32_GPIO_FUNCTION(AVR32_SPI1_MISO_0_FUNCTION));
+        GpioPinConfigSet(AVR32_GPIO_BANK(AVR32_SPI1_MOSI_0_PIN), AVR32_GPIO_PIN(AVR32_SPI1_MOSI_0_PIN),
+                         AVR32_GPIO_FUNCTION(AVR32_SPI1_MOSI_0_FUNCTION));
+        GpioPinConfigSet(AVR32_GPIO_BANK(AVR32_SPI1_NPCS_0_PIN), AVR32_GPIO_PIN(AVR32_SPI1_NPCS_0_PIN),
+                         AVR32_GPIO_FUNCTION(AVR32_SPI1_NPCS_0_FUNCTION));
 
-		/* If the mode update bit is set, then update our registers. */
-		if (node->node_mode & SPI_MODE_UPDATE) {
-			Avr32SpiSetup(node);
-		}
+        /* If the mode update bit is set, then update our registers. */
+        if (node->node_mode & SPI_MODE_UPDATE) {
+            Avr32SpiSetup(node);
+        }
 
-		/* Set SPI mode. */
-		outr(AVR32_SPI1_ADDRESS + AVR32_SPI_MR, spireg->mr);
-		//outr(AVR32_SPI1_ADDRESS + AVR32_SPI_CSR0 + (node->node_cs * (AVR32_SPI_CSR1 - AVR32_SPI_CSR0)), spireg->csr);
-		outr(AVR32_SPI1_ADDRESS + AVR32_SPI_CSR0, spireg->csr);
+        /* Set SPI mode. */
+        outr(AVR32_SPI1_ADDRESS + AVR32_SPI_MR, spireg->mr);
+        //outr(AVR32_SPI1_ADDRESS + AVR32_SPI_CSR0 + (node->node_cs * (AVR32_SPI_CSR1 - AVR32_SPI_CSR0)), spireg->csr);
+        outr(AVR32_SPI1_ADDRESS + AVR32_SPI_CSR0, spireg->csr);
 
-		/* Enable SPI. */
-		AVR32_SPI1.cr |= AVR32_SPI_CR_SPIEN_MASK;
+        /* Enable SPI. */
+        AVR32_SPI1.cr |= AVR32_SPI_CR_SPIEN_MASK;
 
-		/* Finally activate the node's chip select. */
-		rc = Avr32Spi1ChipSelect(node->node_cs, (node->node_mode & SPI_MODE_CSHIGH) != 0);
-		if (rc) {
-			/* Release the bus in case of an error. */
-			NutEventPost(&node->node_bus->bus_mutex);
-		}
-	}
-	return rc;
+        /* Finally activate the node's chip select. */
+        rc = Avr32Spi1ChipSelect(node->node_cs, (node->node_mode & SPI_MODE_CSHIGH) != 0);
+        if (rc) {
+            /* Release the bus in case of an error. */
+            NutEventPost(&node->node_bus->bus_mutex);
+        }
+    }
+    return rc;
 }
 
 /*! \brief Deselect a device on the first SPI bus.
@@ -242,48 +286,48 @@ int Avr32SpiBus1Select(NUTSPINODE * node, uint32_t tmo)
 */
 int Avr32SpiBus1Deselect(NUTSPINODE * node)
 {
-	/* Sanity check. */
-	NUTASSERT(node != NULL);
-	NUTASSERT(node->node_bus != NULL);
+    /* Sanity check. */
+    NUTASSERT(node != NULL);
+    NUTASSERT(node->node_bus != NULL);
 
 #ifdef SPIBUS1_DOUBLE_BUFFER
-	Avr32SpiBusWait(node, NUT_WAIT_INFINITE);
+    Avr32SpiBusWait(node, NUT_WAIT_INFINITE);
 #endif
-	/* Deactivate the node's chip select. */
-	Avr32Spi1ChipSelect(node->node_cs, (node->node_mode & SPI_MODE_CSHIGH) == 0);
+    /* Deactivate the node's chip select. */
+    Avr32Spi1ChipSelect(node->node_cs, (node->node_mode & SPI_MODE_CSHIGH) == 0);
 
-	/* Release the bus. */
-	NutEventPost(&node->node_bus->bus_mutex);
+    /* Release the bus. */
+    NutEventPost(&node->node_bus->bus_mutex);
 
-	return 0;
+    return 0;
 }
 
 #if !defined(SPIBUS1_POLLING_MODE) || !defined(SPIBUS1_DOUBLE_BUFFER)
 
-static uint8_t * volatile spi1_txp;
-static uint8_t * volatile spi1_rxp;
+static uint8_t *volatile spi1_txp;
+static uint8_t *volatile spi1_rxp;
 static volatile size_t spi1_xc;
 
 void Avr32SpiBus1Interrupt(void *arg)
 {
-	uint8_t b;
+    uint8_t b;
 
-	/* Get the received byte. */
-	b = (uint8_t)inr(AVR32_SPI1_ADDRESS + AVR32_SPI_RDR) >> AVR32_SPI_RDR_RD_OFFSET;
-	if (spi1_xc) {
-		if (spi1_rxp) {
-			*spi1_rxp++ = b;
-		}
-		spi1_xc--;
-	}
-	if (spi1_xc) {
-		if (spi1_txp) {
-			b = *spi1_txp++;
-		}
-		outr(AVR32_SPI1_ADDRESS + AVR32_SPI_TDR, (b << AVR32_SPI_TDR_TD_OFFSET));
-	} else {
-		NutEventPostFromIrq((void **)arg);
-	}
+    /* Get the received byte. */
+    b = (uint8_t) inr(AVR32_SPI1_ADDRESS + AVR32_SPI_RDR) >> AVR32_SPI_RDR_RD_OFFSET;
+    if (spi1_xc) {
+        if (spi1_rxp) {
+            *spi1_rxp++ = b;
+        }
+        spi1_xc--;
+    }
+    if (spi1_xc) {
+        if (spi1_txp) {
+            b = *spi1_txp++;
+        }
+        outr(AVR32_SPI1_ADDRESS + AVR32_SPI_TDR, (b << AVR32_SPI_TDR_TD_OFFSET));
+    } else {
+        NutEventPostFromIrq((void **) arg);
+    }
 }
 
 /*! 
@@ -302,30 +346,30 @@ void Avr32SpiBus1Interrupt(void *arg)
 */
 int Avr32SpiBus1Transfer(NUTSPINODE * node, CONST void *txbuf, void *rxbuf, int xlen)
 {
-	uint8_t b = 0xff;
-	uintptr_t base;
+    uint8_t b = 0xff;
+    uintptr_t base;
 
-	/* Sanity check. */
-	NUTASSERT(node != NULL);
-	NUTASSERT(node->node_bus != NULL);
-	NUTASSERT(node->node_bus->bus_base != 0);
-	base = node->node_bus->bus_base;
+    /* Sanity check. */
+    NUTASSERT(node != NULL);
+    NUTASSERT(node->node_bus != NULL);
+    NUTASSERT(node->node_bus->bus_base != 0);
+    base = node->node_bus->bus_base;
 
-	if (xlen) {
-		spi1_txp = (uint8_t *) txbuf;
-		spi1_rxp = (uint8_t *) rxbuf;
-		spi1_xc = (size_t) xlen;
-		if (spi1_txp) {
-			b = *spi1_txp++;
-		}
-		/* Enable and kick interrupts. */
-		outr(base + AVR32_SPI_IER, AVR32_SPI_IER_RDRF_MASK);
-		outr(base + AVR32_SPI_TDR, (b << AVR32_SPI_TDR_TD_OFFSET));
-		/* Wait until transfer has finished. */
-		NutEventWait(&node->node_bus->bus_ready, NUT_WAIT_INFINITE);
-		outr(base + AVR32_SPI_IDR, (unsigned int) - 1);
-	}
-	return 0;
+    if (xlen) {
+        spi1_txp = (uint8_t *) txbuf;
+        spi1_rxp = (uint8_t *) rxbuf;
+        spi1_xc = (size_t) xlen;
+        if (spi1_txp) {
+            b = *spi1_txp++;
+        }
+        /* Enable and kick interrupts. */
+        outr(base + AVR32_SPI_IER, AVR32_SPI_IER_RDRF_MASK);
+        outr(base + AVR32_SPI_TDR, (b << AVR32_SPI_TDR_TD_OFFSET));
+        /* Wait until transfer has finished. */
+        NutEventWait(&node->node_bus->bus_ready, NUT_WAIT_INFINITE);
+        outr(base + AVR32_SPI_IDR, (unsigned int) -1);
+    }
+    return 0;
 }
 #endif
 
@@ -333,26 +377,26 @@ int Avr32SpiBus1Transfer(NUTSPINODE * node, CONST void *txbuf, void *rxbuf, int 
 * \brief Avr32 SPI bus driver implementation structure.
 */
 NUTSPIBUS spiBus1Avr32 = {
-	NULL,                       /*!< Bus mutex semaphore (bus_mutex). */
-	NULL,                       /*!< Bus ready signal (bus_ready). */
-	AVR32_SPI1_ADDRESS,          /*!< Bus base address (bus_base). */
-	&sig_SPI1,                  /*!< Bus interrupt handler (bus_sig). */
-	Avr32SpiBusNodeInit,         /*!< Initialize the bus (bus_initnode). */
-	Avr32SpiBus1Select,          /*!< Select the specified device (bus_alloc). */
-	Avr32SpiBus1Deselect,        /*!< Deselect the specified device (bus_release). */
+    NULL,                       /*!< Bus mutex semaphore (bus_mutex). */
+    NULL,                       /*!< Bus ready signal (bus_ready). */
+    AVR32_SPI1_ADDRESS,         /*!< Bus base address (bus_base). */
+    &sig_SPI1,                  /*!< Bus interrupt handler (bus_sig). */
+    Avr32SpiBusNodeInit,        /*!< Initialize the bus (bus_initnode). */
+    Avr32SpiBus1Select,         /*!< Select the specified device (bus_alloc). */
+    Avr32SpiBus1Deselect,       /*!< Deselect the specified device (bus_release). */
 #if defined(SPIBUS1_POLLING_MODE)
-	Avr32SpiBusPollTransfer,     /*!< Transfer data to and from a specified node (bus_transfer). */
+    Avr32SpiBusPollTransfer,    /*!< Transfer data to and from a specified node (bus_transfer). */
 #elif defined(SPIBUS1_DOUBLE_BUFFER)
-	Avr32SpiBusDblBufTransfer,
+    Avr32SpiBusDblBufTransfer,
 #else
-	Avr32SpiBus1Transfer,
+    Avr32SpiBus1Transfer,
 #endif
 #ifdef SPIBUS1_DOUBLE_BUFFER
-	Avr32SpiBusWait,
+    Avr32SpiBusWait,
 #else
-	NutSpiBusWait,              /*!< Wait for bus transfer ready (bus_wait). */
+    NutSpiBusWait,              /*!< Wait for bus transfer ready (bus_wait). */
 #endif
-	NutSpiBusSetMode,           /*!< Set SPI mode of a specified device (bus_set_mode). */
-	NutSpiBusSetRate,           /*!< Set clock rate of a specified device (bus_set_rate). */
-	NutSpiBusSetBits            /*!< Set number of data bits of a specified device (bus_set_bits). */
+    NutSpiBusSetMode,           /*!< Set SPI mode of a specified device (bus_set_mode). */
+    NutSpiBusSetRate,           /*!< Set clock rate of a specified device (bus_set_rate). */
+    NutSpiBusSetBits            /*!< Set number of data bits of a specified device (bus_set_bits). */
 };
