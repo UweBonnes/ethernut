ef1d9efcffda006c9ed110530bfbd556b8016e12
haraldkipp <haraldkipp@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2009-12-21 17:51:26 +0000

    Caltime sample version 2.0: Added system uptime, support line editing.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@2844 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- nut/app/caltime/caltime.c
+++ nut/app/caltime/caltime.c
@@ -1,5 +1,8 @@
 /*!
- * Copyright (C) 2001-2005 by egnite Software GmbH. All rights reserved.
+ * Copyright (C) 2001-2005 by egnite Software GmbH
+ * Copyright (C) 2009 by egnite GmbH
+ *
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -14,11 +17,11 @@
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY EGNITE SOFTWARE GMBH AND CONTRIBUTORS
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL EGNITE
- * SOFTWARE GMBH OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
  * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
  * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
@@ -31,10 +34,7 @@
  */
 
 /*!
- * $Log$
- * Revision 1.1  2007/06/03 08:48:17  haraldkipp
- * New application sample demonstrates calendar functions.
- *
+ * $Id$
  */
 
 #include <dev/board.h>
@@ -49,13 +49,24 @@
 #include <string.h>
 #include <time.h>
 
+/* Remove the following line to exclude the line editor. */
+#define USE_LINE_EDITOR
+
+#ifdef USE_LINE_EDITOR
+#include <gorp/edline.h>
+#define LINE_MAXLENGTH  80
+static char edbuff[LINE_MAXLENGTH];
+static EDLINE *edline;
+#endif
+
 /*!
  * \example caltime/caltime.c
  *
  * Demonstrates Nut/OS date and time functions, which had been contributed
  * by Oliver Schulz.
  */
-static char *version = "1.0";
+static char *version = "2.0";
+
 
 /* Used for ASCII Art Animation. */
 static char *rotor = "|/-\\";
@@ -70,11 +81,11 @@ static char *weekday_name[7] = {
 static void PrintDateTime(CONST struct _tm *stm)
 {
     printf("%s, %04d/%02d/%02d, %02d:%02d:%02d%s"
-        , weekday_name[stm->tm_wday]
-        , stm->tm_year + 1900, stm->tm_mon + 1, stm->tm_mday
-        , stm->tm_hour, stm->tm_min, stm->tm_sec
-        , stm->tm_isdst ? " DST" : ""
-        );
+           , weekday_name[stm->tm_wday]
+           , stm->tm_year + 1900, stm->tm_mon + 1, stm->tm_mday
+           , stm->tm_hour, stm->tm_min, stm->tm_sec
+           , stm->tm_isdst ? " DST" : ""
+           );
 }
 
 /*
@@ -84,39 +95,37 @@ static void PrintDateTime(CONST struct _tm *stm)
  */
 static int EnterDate(struct _tm *stm)
 {
-    int year;
-    int mon;
-    int mday;
-    int n;
+    int year = stm->tm_year + 1900;
+    int month = stm->tm_mon + 1;
+    int day = stm->tm_mday;
 
-    printf("Enter date, use format YYYY/MM/DD (%04d/%02d/%02d): ", 
-        stm->tm_year + 1900, stm->tm_mon + 1, stm->tm_mday);
-    n = scanf("%d/%d/%d", &year, &mon, &mday);
-    if (n >= 1) {
-        printf("%04d", year);
-        if (year < 1970 || year > 2038) {
-            printf("\nBad year: %d is not within range 1970..2038\n", year);
-            return -1;
-        }
-        stm->tm_year = year - 1900;
-    }
-    if (n >= 2) {
-        printf("/%02d", mon);
-        if (mon < 1 || mon > 31) {
-            printf("\nBad month: %d is not within range 1..12\n", mon);
-            return -1;
-        }
-        stm->tm_mon = mon - 1;
-    }
-    if (n >= 3) {
-        printf("/%02d", mday);
-        if (mday < 1 || mday > 31) {
-            printf("\nBad day: %d is not within range 1..31\n", mday);
-            return -1;
-        }
-        stm->tm_mday = mday;
-    }
+    printf("Enter date, use format YYYY/MM/DD");
+#ifdef USE_LINE_EDITOR
+    printf("  : ");
+    sprintf(edbuff, "%04d/%02d/%02d", year, month, day);
+    EdLineRead(edline, edbuff, LINE_MAXLENGTH);
+    sscanf(edbuff, "%d/%d/%d", &year, &month, &day);
+#else
+    printf(" (%04d/%02d/%02d): ", year, month, day);
+    scanf("%d/%d/%d", &year, &month, &day);
     putchar('\n');
+#endif
+
+    if (year < 1970 || year > 2038) {
+        printf("Bad year: %d is not within range 1970..2038\n", year);
+        return -1;
+    }
+    if (month < 1 || month > 12) {
+        printf("Bad month: %d is not within range 1..12\n", month);
+        return -1;
+    }
+    if (day < 1 || day > 31) {
+        printf("Bad day: %d is not within range 1..31\n", day);
+        return -1;
+    }
+    stm->tm_year = year - 1900;
+    stm->tm_mon = month - 1;
+    stm->tm_mday = day;
 
     return 0;
 }
@@ -128,39 +137,37 @@ static int EnterDate(struct _tm *stm)
  */
 static int EnterTime(struct _tm *stm)
 {
-    int hour;
-    int minute;
-    int second;
-    int n;
+    int hour = stm->tm_hour;
+    int minute = stm->tm_min;
+    int second = stm->tm_sec;
 
-    printf("Enter time, use 24h format HH:MM:SS (%02d:%02d:%02d): ", 
-        stm->tm_hour, stm->tm_min, stm->tm_sec);
-    n = scanf("%d:%d:%d", &hour, &minute, &second);
-    if (n >= 1) {
-        printf("%02d", hour);
-        if (hour < 0 || hour > 23) {
-            printf("\nBad hour: %d is not within range 0..23\n", hour);
-            return -1;
-        }
-        stm->tm_hour = hour;
-    }
-    if (n >= 2) {
-        printf(":%02d", minute);
-        if (minute < 0 || minute > 59) {
-            printf("\nBad minute: %d is not within range 0..59\n", minute);
-            return -1;
-        }
-        stm->tm_min = minute;
-    }
-    if (n >= 3) {
-        printf(":%02d", second);
-        if (second < 0 || second > 59) {
-            printf("\nBad second: %d is not within range 0..59\n", second);
-            return -1;
-        }
-        stm->tm_sec = second;
-    }
+    printf("Enter time, use 24h format HH:MM:SS");
+#ifdef USE_LINE_EDITOR
+    printf(": ");
+    sprintf(edbuff, "%02d:%02d:%02d", hour, minute, second);
+    EdLineRead(edline, edbuff, LINE_MAXLENGTH);
+    sscanf(edbuff, "%d:%d:%d", &hour, &minute, &second);
+#else
+    printf(" (%02d:%02d:%02d): ", hour, minute, second);
+    scanf("%d:%d:%d", &hour, &minute, &second);
     putchar('\n');
+#endif
+
+    if (hour < 0 || hour > 23) {
+        printf("Bad hour: %d is not within range 0..23\n", hour);
+        return -1;
+    }
+    if (minute < 0 || minute > 59) {
+        printf("Bad minute: %d is not within range 0..59\n", minute);
+        return -1;
+    }
+    if (second < 0 || second > 59) {
+        printf("Bad second: %d is not within range 0..59\n", second);
+        return -1;
+    }
+    stm->tm_hour = hour;
+    stm->tm_min = minute;
+    stm->tm_sec = second;
 
     return 0;
 }
@@ -177,30 +184,32 @@ static long EnterTimeDiff(long init)
     long hours;
     long minutes;
     long seconds;
-    int n;
 
     seconds = init;
     minutes = seconds / 60L;
     hours = minutes / 60L;
     minutes = abs(minutes % 60L);
-    printf("Enter time difference in format HH:MM (%+03ld:%02ld): ", hours, minutes);
-    n = scanf("%ld:%ld", &hours, &minutes);
-    if (n >= 1) {
-        printf("%+03ld", hours);
-        if (hours < -12 || hours > 12) {
-            printf("\nBad hours: %ld is not within range -12..+12\n", hours);
-            return init;
-        }
-    }
-    if (n >= 2) {
-        printf("%02ld", minutes);
-        if (minutes < 0 || minutes > 59) {
-            printf("\nBad minutes: %ld is not within range 0..59\n", minutes);
-            return init;
-        }
-    }
-    putchar('\n');
 
+    printf("Enter time difference in format HH:MM");
+#ifdef USE_LINE_EDITOR
+    printf(": ");
+    sprintf(edbuff, "%+03ld:%02ld", hours, minutes);
+    EdLineRead(edline, edbuff, LINE_MAXLENGTH);
+    sscanf(edbuff, "%ld:%ld", &hours, &minutes);
+#else
+    printf(" (%+03ld:%02ld): ", hours, minutes);
+    scanf("%ld:%ld", &hours, &minutes);
+    putchar('\n');
+#endif
+
+    if (hours < -12 || hours > 12) {
+        printf("\nBad hours: %ld is not within range -12..+12\n", hours);
+        return init;
+    }
+    if (minutes < 0 || minutes > 59) {
+        printf("\nBad minutes: %ld is not within range 0..59\n", minutes);
+        return init;
+    }
     return (hours * 60L + minutes) * 60L;
 }
 
@@ -211,10 +220,12 @@ static long EnterTimeDiff(long init)
  */
 static void DisplaySeconds(void)
 {
-    int i = 0;
+    uint_fast8_t i = 0;
 
     while (!kbhit()) {
-        printf(" [%c] Seconds since epoch: %ld\r", rotor[++i & 3], (long)time(NULL));
+        printf(" [%c] Seconds since epoch: %ld\r"
+               , rotor[++i & 3]
+               , (long) time(NULL));
         NutSleep(200);
     }
     putchar('\n');
@@ -229,7 +240,7 @@ static void DisplayZuluTime(void)
 {
     time_t secs;
     struct _tm *gmt;
-    int i = 0;
+    uint_fast8_t i = 0;
 
     while (!kbhit()) {
         secs = time(NULL);
@@ -251,7 +262,7 @@ static void DisplayLocalTime(void)
 {
     time_t tt;
     struct _tm *ltm;
-    int i = 0;
+    uint_fast8_t i = 0;
 
     while (!kbhit()) {
         /* Get local time and print it. */
@@ -275,6 +286,32 @@ static void DisplayLocalTime(void)
 }
 
 /*
+ * Display system up time.
+ *
+ * The value is constantly updated until the user presses a key.
+ */
+static void DisplayUpTime(void)
+{
+    uint32_t hours;
+    uint32_t minutes;
+    uint32_t seconds;
+    uint_fast8_t i = 0;
+
+    while (!kbhit()) {
+        seconds = NutGetSeconds();
+        minutes = seconds / 60UL;
+        hours = minutes / 60UL;
+        minutes %= 60UL;
+        seconds %= 60UL;
+        printf(" [%c] System is running %lu hours"
+               ", %lu minutes and %lu seconds   \r"
+               , rotor[++i & 3], hours, minutes, seconds);
+        NutSleep(500);
+    }
+    putchar('\n');
+}
+
+/*
  * Display the week day of a queried calendar date.
  *
  * mktime() updates the structure members tm_yday and tm_wday.
@@ -285,7 +322,7 @@ static void CalcWeekDay(void)
 {
     struct _tm date;
     time_t secs;
-    
+
     /* Use current local time as default. */
     time(&secs);
     memcpy(&date, localtime(&secs), sizeof(date));
@@ -333,15 +370,30 @@ static void SetLocalTime(void)
  */
 int main(void)
 {
-    u_long baud = 115200;
+    uint32_t baud = 115200;
     int cmd;
 
     /* Use UART device for stdin and stdout. */
     NutRegisterDevice(&DEV_UART, 0, 0);
     freopen(DEV_UART_NAME, "w", stdout);
-    freopen(DEV_UART_NAME, "r", stdin);    
+    freopen(DEV_UART_NAME, "r", stdin);
     _ioctl(_fileno(stdout), UART_SETSPEED, &baud);
-    printf("\n\nCalendar Time %s running on Nut/OS %s\n", version, NutVersionString());
+    printf("\n\nCalendar Time %s running on Nut/OS %s\n"
+           , version, NutVersionString());
+
+#ifdef USE_LINE_EDITOR
+    /* Open line editor, if configured. */
+    printf("Opening line editor...");
+    edline = EdLineOpen(EDIT_MODE_ECHO);
+    if (edline) {
+        puts("OK");
+    } else {
+        puts("failed");
+    }
+#else
+    puts("Note: Enable local echo!");
+#endif
+
 
 #ifdef RTC_CHIP
     /* Register and query hardware RTC, if available. */
@@ -349,13 +401,12 @@ int main(void)
     if (NutRegisterRtc(&RTC_CHIP)) {
         puts("failed");
     } else {
-        u_long rtc_stat;
+        uint32_t rtc_stat;
 
         NutRtcGetStatus(&rtc_stat);
         if (rtc_stat & RTC_STATUS_PF) {
             puts("power failure");
-        }
-        else {
+        } else {
             puts("OK");
         }
     }
@@ -366,7 +417,8 @@ int main(void)
         puts("\n  0 - Display seconds counter");
         puts("  1 - Display universal time");
         puts("  2 - Display local time");
-        puts("  3 - Calculate weekday");
+        puts("  3 - Display system uptime");
+        puts("  C - Calculate weekday");
         puts("  S - Set local time");
         puts("  Y - Toggle DST calculation");
         puts("  Z - Set timezone");
@@ -380,11 +432,10 @@ int main(void)
 
         /* Get the next command. */
         cmd = getchar();
-        putchar(cmd);
         putchar('\n');
 
         /* Process the command. */
-        switch(cmd) {
+        switch (cmd) {
         case '0':
             DisplaySeconds();
             break;
@@ -395,6 +446,9 @@ int main(void)
             DisplayLocalTime();
             break;
         case '3':
+            DisplayUpTime();
+            break;
+        case 'C':
             CalcWeekDay();
             break;
         case 'S':
@@ -416,4 +470,3 @@ int main(void)
     }
     return 0;
 }
-
