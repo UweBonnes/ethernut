7987272967744982122502b96f99273a21ba88cc
haraldkipp <haraldkipp@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2010-05-11 15:48:52 +0000

    Corrected a few typos, added a few LSB definitions. Replaces CVS Log by SVN Id keyword.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@3007 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- nut/include/arch/arm/at91_udp.h
+++ nut/include/arch/arm/at91_udp.h
@@ -41,14 +41,7 @@
  * \brief AT91 peripherals (USB device port).
  *
  * \verbatim
- *
- * $Log$
- * Revision 1.1  2007/07/13 15:21:36  olereinhardt
- * 2007-07-13  Ole Reinhardt <ole.reinhardt@embedded-it.de>
- *         * include/arch/arm/at91sam7x.h: Added UDP support
- *         * include/arch/arm/at91_udp.h: Defines for arm7 UDP controller
- * *
- *
+ * $Id$
  * \endverbatim
  */
 
@@ -59,138 +52,123 @@
 
 /*! \name Frame Number Register */
 /*@{*/
-#define UDP_NUM_OFF      0x00000000
-#define UDP_NUM          (UDP_BASE + UDP_NUM_OFF)
-#define UDP_FRM_NUM      (0x07FF <<  0) /*!< \brief Mask: Frame Number as Defined in the Packet Field Formats */
-#define UDP_FRM_ERR      _BV(16)        /*!< \brief Frame Error */
-#define UDP_FRM_OK       _BV(17)        /*!< \brief Frame OK */
+#define UDP_NUM_OFF      0x00000000     /*!< \brief Frame number register offset. */
+#define UDP_NUM          (UDP_BASE + UDP_NUM_OFF) /*!< \brief Frame number register address. */
+#define UDP_FRM_NUM      0x000007FF     /*!< \brief Current frame number mask. */
+#define UDP_FRM_NUM_LSB  0              /*!< \brief Current frame number LSB. */
+#define UDP_FRM_ERR      _BV(16)        /*!< \brief Frame error. */
+#define UDP_FRM_OK       _BV(17)        /*!< \brief Frame OK. */
 /*@}*/
 
 /*! \name Global State Register */
 /*@{*/
-#define UDP_GLBSTATE_OFF 0x00000004
-#define UDP_GLBSTATE     (UDP_BASE + UDP_GLBSTATE_OFF)
-#define UDP_FADDEN       _BV(0)         /*!< \brief Function Address Enable */
-#define UDP_CONFG        _BV(1)         /*!< \brief Configured */
+#define UDP_GLBSTATE_OFF 0x00000004     /*!< \brief Global state register offset. */
+#define UDP_GLBSTATE     (UDP_BASE + UDP_GLBSTATE_OFF) /*!< \brief Global state register address. */
+#define UDP_FADDEN       _BV(0)         /*!< \brief Device is in address state. */
+#define UDP_CONFG        _BV(1)         /*!< \brief Device is in configured state. */
 /* TODO: Not defined in datasheet for AT91SAM7X... */
-//#define UDP_ESR          _BV(2)         /*!< \brief Enable Send Resume */
-//#define UDP_RSMINPR      _BV(3)         /*!< \brief A Resume Has Been Sent to the Host */
-//#define UDP_RMWUPE       _BV(4)         /*!< \brief Remote Wake Up Enable */
+//#define UDP_ESR          _BV(2)         /*!< \brief Enable send resume. */
+//#define UDP_RSMINPR      _BV(3)         /*!< \brief A resume has been sent to the host. */
+//#define UDP_RMWUPE       _BV(4)         /*!< \brief Remote wake up enable. */
 /*@}*/
 
 /*! \name Function Address Register */
 /*@{*/
-#define UDP_FADDR_OFF    0x00000008
-#define UDP_FADDR        (UDP_BASE + UDP_FADDR_OFF)
-#define UDP_FADD         (0x7F << 0)    /*!< \brief Mask: Function Address Value */
-#define UDP_FEN          _BV(8)         /*!< \brief Function Enable */
+#define UDP_FADDR_OFF    0x00000008     /*!< \brief Function address register offset. */
+#define UDP_FADDR        (UDP_BASE + UDP_FADDR_OFF) /*!< \brief Function address register address. */
+#define UDP_FADD         0x0000007F     /*!< \brief Mask: Function address value. */
+#define UDP_FEN          _BV(8)         /*!< \brief Function endpoint enabled. */
 /*@}*/
 
-/*! \name Interrup Enable Register */
+/*! \name Interrupt Registers */
 /*@{*/
-#define UDP_IER_OFF      0x00000010 
-#define UDP_IER          (UDP_BASE + UDP_IER_OFF)
-
-#define UDP_EPINT0       _BV( 0)        /*!< \brief Endpoint 0 Interrupt */
-#define UDP_EPINT1       _BV( 1)        /*!< \brief Endpoint 0 Interrupt */
-#define UDP_EPINT2       _BV( 2)        /*!< \brief Endpoint 2 Interrupt */
-#define UDP_EPINT3       _BV( 3)        /*!< \brief Endpoint 3 Interrupt */
-#define UDP_EPINT4       _BV( 4)        /*!< \brief Endpoint 4 Interrupt */
-#define UDP_EPINT5       _BV( 5)        /*!< \brief Endpoint 5 Interrupt */
-#define UDP_RXSUSP       _BV( 8)        /*!< \brief USB Suspend Interrupt */
-#define UDP_RXRSM        _BV( 9)        /*!< \brief USB Resume Interrupt */
-#define UDP_EXTRSM       _BV(10)        /*!< \brief USB External Resume Interrupt */
-#define UDP_SOFINT       _BV(11)        /*!< \brief USB Start Of frame Interrupt */
-#define UDP_ENDBUSRES    _BV(12)        /*!< \brief USB End Of Bus Reset Interrupt */
-#define UDP_WAKEUP       _BV(13)        /*!< \brief USB Resume Interrupt */
-/*@}*/
-
-/*! \name Interrup Disable Register */
-/*@{*/
-#define UDP_IDR_OFF      0x00000014
-#define UDP_IDR          (UDP_BASE + UDP_IDR_OFF)
-/*@}*/
-
-/*! \name Interrup Mask Register */
-/*@{*/
-#define UDP_IMR_OFF      0x00000018
-#define UDP_IMR          (UDP_BASE + UDP_IMR_OFF)
-/*@}*/
-
-/*! \name Interrup Status Register */
-/*@{*/
-#define UDP_ISR_OFF      0x0000001C
-#define UDP_ISR          (UDP_BASE + UDP_ISR_OFF)
-/*@}*/
-
-/*! \name Interrup Clear Register */
-/*@{*/
-#define UDP_ICR_OFF      0x00000020
-#define UDP_ICR          (UDP_BASE + UDP_ICR_OFF)
+#define UDP_IER_OFF      0x00000010     /*!< \brief Interrupt enable register offset. */
+#define UDP_IER          (UDP_BASE + UDP_IER_OFF) /*!< \brief Interrupt enable register address. */
+#define UDP_IDR_OFF      0x00000014     /*!< \brief Interrupt disable register offset. */
+#define UDP_IDR          (UDP_BASE + UDP_IDR_OFF) /*!< \brief Interrupt disable register address. */
+#define UDP_IMR_OFF      0x00000018     /*!< \brief Interrupt mask register offset. */
+#define UDP_IMR          (UDP_BASE + UDP_IMR_OFF) /*!< \brief Interrupt mask register address. */
+#define UDP_ISR_OFF      0x0000001C     /*!< \brief Interrupt status register offset. */
+#define UDP_ISR          (UDP_BASE + UDP_ISR_OFF) /*!< \brief Interrupt status register address. */
+#define UDP_ICR_OFF      0x00000020     /*!< \brief Interrupt clear register offset. */
+#define UDP_ICR          (UDP_BASE + UDP_ICR_OFF) /*!< \brief Interrupt clear register address. */
+#define UDP_EPINT0       _BV( 0)        /*!< \brief Endpoint 0 interrupt. */
+#define UDP_EPINT1       _BV( 1)        /*!< \brief Endpoint 0 interrupt. */
+#define UDP_EPINT2       _BV( 2)        /*!< \brief Endpoint 2 interrupt. */
+#define UDP_EPINT3       _BV( 3)        /*!< \brief Endpoint 3 interrupt. */
+#define UDP_EPINT4       _BV( 4)        /*!< \brief Endpoint 4 interrupt. */
+#define UDP_EPINT5       _BV( 5)        /*!< \brief Endpoint 5 interrupt. */
+#define UDP_RXSUSP       _BV( 8)        /*!< \brief USB suspend interrupt. */
+#define UDP_RXRSM        _BV( 9)        /*!< \brief USB resume interrupt. */
+#define UDP_EXTRSM       _BV(10)        /*!< \brief USB external resume interrupt. */
+#define UDP_SOFINT       _BV(11)        /*!< \brief USB start of frame interrupt. */
+#define UDP_ENDBUSRES    _BV(12)        /*!< \brief USB end of bus reset interrupt. */
+#define UDP_WAKEUP       _BV(13)        /*!< \brief USB resume interrupt. */
 /*@}*/
 
 /*! \name Reset Endpoint Register */
 /*@{*/
-#define UDP_RST_EP_OFF   0x00000028
-#define UDP_RST_EP       (UDP_BASE + UDP_RST_EP_OFF)
+#define UDP_RST_EP_OFF   0x00000028     /*!< \brief Reset endpoint register offset. */
+#define UDP_RST_EP       (UDP_BASE + UDP_RST_EP_OFF) /*!< \brief Reset endpoint register address. */
 
-#define UDP_EP0          _BV(0)          /*!< \brief Reset Endpoint 0 */
-#define UDP_EP1          _BV(1)          /*!< \brief Reset Endpoint 1 */
-#define UDP_EP2          _BV(2)          /*!< \brief Reset Endpoint 2 */
-#define UDP_EP3          _BV(3)          /*!< \brief Reset Endpoint 3 */
-#define UDP_EP4          _BV(4)          /*!< \brief Reset Endpoint 3 */
-#define UDP_EP5          _BV(5)          /*!< \brief Reset Endpoint 3 */
+#define UDP_EP0          _BV(0)          /*!< \brief Reset endpoint 0. */
+#define UDP_EP1          _BV(1)          /*!< \brief Reset endpoint 1. */
+#define UDP_EP2          _BV(2)          /*!< \brief Reset endpoint 2. */
+#define UDP_EP3          _BV(3)          /*!< \brief Reset endpoint 3. */
+#define UDP_EP4          _BV(4)          /*!< \brief Reset endpoint 4. */
+#define UDP_EP5          _BV(5)          /*!< \brief Reset endpoint 5. */
 /*@}*/
 
-/*! \name Endpoint Control and Status Register */
+/*! \name Endpoint Control and Status Registers */
 /*@{*/
-#define UDP_CSR_OFF      0x00000030
-#define UDP_CSR          (UDP_BASE + UDP_CSR_OFF)
-#define UDP_EP0_CSR      (UDP_CSR + 0x00000000)  /*!< \brief Endpoint 0 CSR Register */
-#define UDP_EP1_CSR      (UDP_CSR + 0x00000004)  /*!< \brief Endpoint 1 CSR Register */
-#define UDP_EP2_CSR      (UDP_CSR + 0x00000008)  /*!< \brief Endpoint 2 CSR Register */
-#define UDP_EP3_CSR      (UDP_CSR + 0x0000000C)  /*!< \brief Endpoint 3 CSR Register */
-#define UDP_EP4_CSR      (UDP_CSR + 0x00000010)  /*!< \brief Endpoint 4 CSR Register */
-#define UDP_EP5_CSR      (UDP_CSR + 0x00000014)  /*!< \brief Endpoint 5 CSR Register */
+#define UDP_CSR_OFF      0x00000030     /*!< \brief Endpoint control and status register offset. */
+#define UDP_CSR          (UDP_BASE + UDP_CSR_OFF) /*!< \brief Endpoint control and status register address. */
+#define UDP_EP0_CSR      (UDP_CSR + 0x00000000)  /*!< \brief Endpoint 0 control and status register address. */
+#define UDP_EP1_CSR      (UDP_CSR + 0x00000004)  /*!< \brief Endpoint 1 control and status register address. */
+#define UDP_EP2_CSR      (UDP_CSR + 0x00000008)  /*!< \brief Endpoint 2 control and status register address. */
+#define UDP_EP3_CSR      (UDP_CSR + 0x0000000C)  /*!< \brief Endpoint 3 control and status register address. */
+#define UDP_EP4_CSR      (UDP_CSR + 0x00000010)  /*!< \brief Endpoint 4 control and status register address. */
+#define UDP_EP5_CSR      (UDP_CSR + 0x00000014)  /*!< \brief Endpoint 5 control and status register address. */
 
-#define UDP_TXCOMP       _BV(0)         /*!< \brief Generates an IN packet with data previously written in the DPR */
-#define UDP_RX_DATA_BK0  _BV(1)         /*!< \brief Receive Data Bank 0 */
-#define UDP_RXSETUP      _BV(2)         /*!< \brief Sends STALL to the Host (Control endpoints) */
-#define UDP_STALL_SEND_ISOERROR _BV(3)  /*!< \brief STALL send / Isochronous error (Isochronous endpoints) */
-#define UDP_TXPKTRDY     _BV(4)         /*!< \brief Transmit Packet Ready */
-#define UDP_FORCESTALL   _BV(5)         /*!< \brief Force Stall (used by Control, Bulk and Isochronous endpoints). */
-#define UDP_RX_DATA_BK1  _BV(6)         /*!< \brief Receive Data Bank 1 (only used by endpoints with ping-pong attributes) */
-#define UDP_DIR          _BV(7)         /*!< \brief Transfer Direction */
-#define UDP_EPTYPE       (0x7 << 8)     /*!< \brief Mask: Endpoint type, 3 BIT */
-#define     UDP_EPTYPE_CTRL      (0x0 << 8) /*!< \brief Endpoint typeControl */
-#define     UDP_EPTYPE_ISO_OUT   (0x1 << 8) /*!< \brief Endpoint type Isochronous OUT */
-#define     UDP_EPTYPE_BULK_OUT  (0x2 << 8) /*!< \brief Endpoint type Bulk OUT */
-#define     UDP_EPTYPE_INT_OUT   (0x3 << 8) /*!< \brief Endpoint type Interrupt OUT */
-#define     UDP_EPTYPE_ISO_IN    (0x5 << 8) /*!< \brief Endpoint type Isochronous IN */
-#define     UDP_EPTYPE_BULK_IN   (0x6 << 8) /*!< \brief Endpoint type Bulk IN */
-#define     UDP_EPTYPE_INT_IN    (0x7 << 8) /*!< \brief Endpoint type Interrupt IN */
-#define UDP_DTGLE       _BV(11)         /*!< \brief Data Toggle */
-#define UDP_EPEDS       _BV(15)         /*!< \brief Endpoint Enable Disable */
-#define UDP_RXBYTECNT   (0x7FF << 16)   /*!< \brief Mask: Number Of Bytes Available in the FIFO */
+#define UDP_TXCOMP       _BV(0)         /*!< \brief Generates an IN packet with data previously written in the DPR. */
+#define UDP_RX_DATA_BK0  _BV(1)         /*!< \brief Receive data bank 0. */
+#define UDP_RXSETUP      _BV(2)         /*!< \brief Sends Stall to the host (control endpoints). */
+#define UDP_STALL_SEND_ISOERROR _BV(3)  /*!< \brief Stall send / isochronous error (isochronous endpoints). */
+#define UDP_TXPKTRDY     _BV(4)         /*!< \brief Transmit packet ready. */
+#define UDP_FORCESTALL   _BV(5)         /*!< \brief Force Stall (used by control, bulk and isochronous endpoints). */
+#define UDP_RX_DATA_BK1  _BV(6)         /*!< \brief Receive data bank 1 (only used by endpoints with ping-pong attributes). */
+#define UDP_DIR          _BV(7)         /*!< \brief Transfer direction. */
+#define UDP_EPTYPE       (0x7 << 8)     /*!< \brief Mask: Endpoint type, 3 BIT. */
+#define     UDP_EPTYPE_CTRL      (0x0 << 8) /*!< \brief Endpoint type control. */
+#define     UDP_EPTYPE_ISO_OUT   (0x1 << 8) /*!< \brief Endpoint type isochronous OUT. */
+#define     UDP_EPTYPE_BULK_OUT  (0x2 << 8) /*!< \brief Endpoint type bulk OUT. */
+#define     UDP_EPTYPE_INT_OUT   (0x3 << 8) /*!< \brief Endpoint type interrupt OUT. */
+#define     UDP_EPTYPE_ISO_IN    (0x5 << 8) /*!< \brief Endpoint type isochronous IN. */
+#define     UDP_EPTYPE_BULK_IN   (0x6 << 8) /*!< \brief Endpoint type bulk IN. */
+#define     UDP_EPTYPE_INT_IN    (0x7 << 8) /*!< \brief Endpoint type interrupt IN. */
+#define UDP_DTGLE       _BV(11)         /*!< \brief Data toggle. */
+#define UDP_EPEDS       _BV(15)         /*!< \brief Endpoint enable disable. */
+#define UDP_RXBYTECNT   0x07FF0000   /*!< \brief Mask: Number of bytes available in the FIFO. */
+#define UDP_RXBYTECNT_LSB   16   /*!< \brief LSB: Number of bytes available in the FIFO. */
 /*@}*/
 
-/*! \name FIFO Data Register */
+/*! \name Endpoint FIFO Data Registers */
 /*@{*/
-#define UDP_FDR_OFF      0x00000050
-#define UDP_FDR          (UDP_BASE + UDP_FDR_OFF)
-#define UDP_EP0_FDR      (UDP_FDR + 0x00000000)  /*!< \brief Endpoint 0 FIFO Data Register */
-#define UDP_EP1_FDR      (UDP_FDR + 0x00000004)  /*!< \brief Endpoint 1 FIFO Data Register */
-#define UDP_EP2_FDR      (UDP_FDR + 0x00000008)  /*!< \brief Endpoint 2 FIFO Data Register */
-#define UDP_EP3_FDR      (UDP_FDR + 0x0000000C)  /*!< \brief Endpoint 3 FIFO Data Register */
-#define UDP_EP4_FDR      (UDP_FDR + 0x00000010)  /*!< \brief Endpoint 4 FIFO Data Register */
-#define UDP_EP5_FDR      (UDP_FDR + 0x00000014)  /*!< \brief Endpoint 5 FIFO Data Register */
+#define UDP_FDR_OFF      0x00000050     /*!< \brief Endpoint FIFO data register offset. */
+#define UDP_FDR          (UDP_BASE + UDP_FDR_OFF) /*!< \brief Endpoint FIFO data register address. */
+#define UDP_EP0_FDR      (UDP_FDR + 0x00000000)  /*!< \brief Endpoint 0 FIFO data register address. */
+#define UDP_EP1_FDR      (UDP_FDR + 0x00000004)  /*!< \brief Endpoint 1 FIFO data register address. */
+#define UDP_EP2_FDR      (UDP_FDR + 0x00000008)  /*!< \brief Endpoint 2 FIFO data register address. */
+#define UDP_EP3_FDR      (UDP_FDR + 0x0000000C)  /*!< \brief Endpoint 3 FIFO data register address. */
+#define UDP_EP4_FDR      (UDP_FDR + 0x00000010)  /*!< \brief Endpoint 4 FIFO data register address. */
+#define UDP_EP5_FDR      (UDP_FDR + 0x00000014)  /*!< \brief Endpoint 5 FIFO data register address. */
 /*@}*/
 
 /*! \name Tranceiver Control Register */
 /*@{*/
-#define UDP_TXVC_OFF     0x00000074
-#define UDP_TXVC         (UDP_BASE + UDP_TXVDIS_OFF)
-#define UDB_TXVDIS       _BV(8)         /*!< \brief Tranceiver disable). */
+#define UDP_TXVC_OFF     0x00000074     /*!< \brief Transceiver control register offset. */
+#define UDP_TXVC         (UDP_BASE + UDP_TXVC_OFF) /*!< \brief Transceiver control register address. */
+#define UDP_TXVDIS       _BV(8)         /*!< \brief Tranceiver disable. */
 /*@}*/
 
 /*@} xgNutArchArmAt91Udp */
