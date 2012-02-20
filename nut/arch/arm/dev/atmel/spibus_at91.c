9ae4aebb1dc2bdef34ae597eaa00e230273b0c8b
haraldkipp <haraldkipp@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2012-02-20 18:41:44 +0000

    Fix warning in AT91 SPI bus driver. Mind boggling ifdefs, which I hopefully got right now.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@3894 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- nut/arch/arm/dev/atmel/spibus_at91.c
+++ nut/arch/arm/dev/atmel/spibus_at91.c
@@ -139,14 +139,14 @@ int At91SpiSetup(NUTSPINODE * node)
 int At91SpiBusNodeInit(NUTSPINODE * node)
 {
     int rc;
-#if !defined(SPIBUS0_POLLING_MODE) || !defined(SPIBUS1_POLLING_MODE) || defined(SPI1_BASE)
+#if !defined(SPIBUS0_POLLING_MODE) || (!defined(SPIBUS1_POLLING_MODE) && defined(SPI1_BASE)) || defined(SPI1_BASE)
     NUTSPIBUS *bus;
 #endif
 
     /* Sanity check. */
     NUTASSERT(node != NULL);
     NUTASSERT(node->node_bus != NULL);
-#if !defined(SPIBUS0_POLLING_MODE) || !defined(SPIBUS1_POLLING_MODE) || defined(SPI1_BASE)
+#if !defined(SPIBUS0_POLLING_MODE) || (!defined(SPIBUS1_POLLING_MODE) && defined(SPI1_BASE)) || defined(SPI1_BASE)
     bus = node->node_bus;
 #endif
 
