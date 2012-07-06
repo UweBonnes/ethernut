UweBonnes/ethernut

commit aeb3213bedb52c0d4f159dfed56e0ec8eef186d5

Discard broadcasts to other networks and remove NUTIPCONF_ICMP_ARPMETHOD. The former fixes bug #3511170.
git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@4206 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

nut/net/ipin.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 by egnite GmbH
+ * Copyright (C) 2008-2012 by egnite GmbH
  * Copyright (C) 2001-2007 by egnite Software GmbH
  * Copyright (c) 1983, 1993 by The Regents of the University of California
  * Copyright (c) 1993 by Digital Equipment Corporation
@@ -90,13 +90,12 @@ int (*ip_demux) (NUTDEVICE *, NETBUF *);
 
 /*!
  * \brief Process incoming IP datagrams.
+ * \internal
  *
  * Datagrams addressed to other destinations and datagrams
  * whose version number is not 4 are silently discarded.
  *
- * \note This routine is called by the Ethernet layer on
- *       incoming IP datagrams. Applications typically do
- *       not call this function.
+ * This routine is called by the Ethernet layer on incoming IP datagrams.
  *
  * \param dev Identifies the device that received this datagram.
  * \param nb  The network buffer received.
@@ -138,14 +137,13 @@ void NutIpInput(NUTDEVICE * dev, NETBUF * nb)
     /* Optional checksum calculation on incoming datagrams. */
 #endif
 
-    /*
-     * Check for broadcast.
-     */
     dst = ip->ip_dst;
     nif = dev->dev_icb;
 
-    if (dst == INADDR_BROADCAST ||
-        (nif->if_local_ip && nif->if_mask != INADDR_BROADCAST && (dst | nif->if_mask) == INADDR_BROADCAST)) {
+    /*
+     * Check for limited broadcast.
+     */
+    if (dst == INADDR_BROADCAST) {
         bcast = 1;
     }
 
@@ -168,30 +166,40 @@ void NutIpInput(NUTDEVICE * dev, NETBUF * nb)
     }
 
     /*
-     * Packet is unicast.
+     * Check device's local IP address.
      */
-    else {
-        bcast = 0;
-        nb->nb_flags |= NBAF_UNICAST;
-
-#ifdef NUTIPCONF_ICMP_ARPMETHOD
-        /*
-         * Silently discard datagrams for other destinations.
-         * However, if we haven't got an IP address yet, we
-         * allow ICMP datagrams to support dynamic IP ARP method,
-         * if this option had been enabled.
-         */
-        if (nif->if_local_ip == 0 && ip->ip_p == IPPROTO_ICMP && (dst & 0xff000000) != 0xff000000 && (dst & 0xff000000) != 0) {
-            NutNetIfSetup(dev, dst, 0, 0);
-        }
-#endif
-        if (nif->if_local_ip && (dst == 0 || dst != nif->if_local_ip)) {
-            NutNetBufFree(nb);
-            return;
-        }
+    else if (nif->if_local_ip == 0) {
+        /* Not yet configured, discard net-directed datagrams. */
+        NutNetBufFree(nb);
+        return;
     }
 
-    /* Check the IP data length. */
+    /*
+     * Check for unicast.
+     */
+    else if (dst == nif->if_local_ip) {
+        bcast = 0;
+        nb->nb_flags |= NBAF_UNICAST;
+    }
+
+    /*
+     * Check for net-directed broadcast.
+     */
+    else if ((dst & ~nif->if_mask) == ~nif->if_mask) {
+        bcast = 1;
+    }
+
+    /*
+     * Not for us, discard silently.
+     */
+    else {
+        NutNetBufFree(nb);
+        return;
+    }
+
+    /*
+     * Calculate IP data length.
+     */
     nb->nb_tp.sz = htons(ip->ip_len);
     if (nb->nb_tp.sz < hdrlen || nb->nb_tp.sz > nb->nb_nw.sz) {
         NutNetBufFree(nb);
@@ -203,6 +211,9 @@ void NutIpInput(NUTDEVICE * dev, NETBUF * nb)
         nb->nb_tp.vp = ((char *) ip) + hdrlen;
     }
 
+    /*
+     * Route valid datagram to the related handler.
+     */
     if (ip_demux == NULL || (*ip_demux) (dev, nb)) {
         switch (ip->ip_p) {
         case IPPROTO_ICMP:

nut/conf/net/net.nut
@@ -244,22 +244,7 @@ nutnet =
             "ipout.c",
             "ipdemux.c",
             "route.c"
-        },
-        options = 
-        {
-            {
-                macro = "NUTIPCONF_ICMP_ARPMETHOD",
-                brief = "Enable ARP Method",
-                description = "Allow IP configuration using ARP method.\n\n"..
-                              "When enabling this function, be aware that DHCP servers "..
-                              "may probe an IP/MAC relationship by sending an ICMP request. "..
-                              "This triggers the Nut/Net ARP method and terminates the DHCP "..
-                              "client, leaving the system with default configurations of "..
-                              "the network mask (255.255.255.0) and default gateway (none).",
-                flavor = "booldata",
-                file = "include/cfg/ip.h"
-            },
-        }        
+        }
     },
     {
         name = "nutnet_icmp",

commit d83cbdb43eaec9d21786b216c95592047bfbf83d

Fixed a bug with DHCP which was introduced with r4206. Some DHCP servers answer with a unicast DHCP_OFFER which was not recognised with the old code. The implementation of NutIpInput() discarded unicast packets as long as the local interface was not yet configured.
According to RFC1542, chapter 3.1.1 DHCP implementations should allow
getting unicast DHCP_OFFERs.

So I modified NutIpInput() to allow receiving unicast packets even if the 
local interface is not yet configured.



git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@4346 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

nut/net/ipin.c
@@ -105,7 +105,7 @@ void NutIpInput(NUTDEVICE * dev, NETBUF * nb)
     IPHDR *ip;
     uint_fast8_t hdrlen;
     uint32_t dst;
-    uint_fast8_t bcast;
+    uint_fast8_t bcast = 0;
     IFNET *nif;
 
     ip = nb->nb_nw.vp;
@@ -168,33 +168,28 @@ void NutIpInput(NUTDEVICE * dev, NETBUF * nb)
     /*
      * Check device's local IP address.
      */
-    else if (nif->if_local_ip == 0) {
-        /* Not yet configured, discard net-directed datagrams. */
-        NutNetBufFree(nb);
-        return;
-    }
+    else if (nif->if_local_ip != 0) {
+        /*
+         * Check for unicast.
+         */
+        if (dst == nif->if_local_ip) {
+            nb->nb_flags |= NBAF_UNICAST;
+        }
 
-    /*
-     * Check for unicast.
-     */
-    else if (dst == nif->if_local_ip) {
-        bcast = 0;
-        nb->nb_flags |= NBAF_UNICAST;
-    }
+        /*
+         * Check for net-directed broadcast.
+         */
+        else if ((dst & ~nif->if_mask) == ~nif->if_mask) {
+            bcast = 1;
+        }
 
-    /*
-     * Check for net-directed broadcast.
-     */
-    else if ((dst & ~nif->if_mask) == ~nif->if_mask) {
-        bcast = 1;
-    }
-
-    /*
-     * Not for us, discard silently.
-     */
-    else {
-        NutNetBufFree(nb);
-        return;
+        /*
+         * Not for us, discard silently.
+         */
+        else {
+            NutNetBufFree(nb);
+            return;
+        }
     }
 
     /*