85d23860d82b19fec2a0bad2c65b16dd2e89bca9
olereinhardt <olereinhardt@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2012-09-13 15:08:36 +0000

    Fix for LPC177x_8x CPUs with more than 100Mhz:
    We have to set the PBOOST bit as long as we are running with more than 100Mhz
    See Errata: Rev. 3 — 1 September 2012, Chapter 3.5 
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@4606 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- nut/arch/cm3/dev/nxp/system_lpc177x_8x.c
+++ nut/arch/cm3/dev/nxp/system_lpc177x_8x.c
@@ -65,8 +65,10 @@ uint32_t Lpc177x_8xGetCrpValue(void)
  */
 void SystemInit (void)
 {
-
-
+	/* We have to set the PBOOST bit as long as we are running with more than 100Mhz
+	 * See Errata: Rev. 3 — 1 September 2012, Chapter 3.5
+	 */
+	LPC_SC->PBOOST = 0x03;
 }
 
 /**
