UweBonnes/ethernut

commit 317a533f3b7adc88171448e442204fb819050ee9

Enable heap debugging in microHTTP lib. This sucks! We should find a better solution than adding memdebug everywhere to get this done.
git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@4724 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

nut/pro/uhttp/utils.c
@@ -41,6 +41,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <memdebug.h>
 
 void HttpUrlUnescape(char *str)
 {

nut/pro/uhttp/envreg.c
@@ -40,6 +40,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <memdebug.h>
 
 extern ISC_LIST(HTTP_ENVVAR_ENTRY) envVarList;
 

nut/pro/uhttp/envvars.c
@@ -46,6 +46,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <memdebug.h>
 
 /*@cond */
 ISC_LIST(HTTP_ENVVAR_ENTRY) envVarList = ISC_LIST_INITIAL_TYPE(HTTP_ENVVAR_ENTRY);

nut/pro/uhttp/mediatypes.c
@@ -55,6 +55,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <memdebug.h>
 
 ISC_LIST(MEDIA_TYPE_ENTRY) mediaTypeList = ISC_LIST_INITIAL_TYPE(MEDIA_TYPE_ENTRY);
 

nut/pro/uhttp/modules/mod_ssi.c
@@ -52,6 +52,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <memdebug.h>
 
 #define SSI_TYPE_FILE    0x01
 #define SSI_TYPE_VIRTUAL 0x02

nut/pro/uhttp/mtreg.c
@@ -43,6 +43,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <memdebug.h>
 
 extern ISC_LIST(MEDIA_TYPE_ENTRY) mediaTypeList;
 

nut/pro/uhttp/os/nut/streamio.c
@@ -54,6 +54,7 @@
 #include <stdarg.h>
 #include <string.h>
 #include <time.h>
+#include <memdebug.h>
 
 #include <pro/uhttp/streamio.h>
 

nut/pro/uhttp/responses.c
@@ -44,6 +44,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <memdebug.h>
 
 typedef struct _HTTP_RESPONSE_STATUS HTTP_RESPONSE_STATUS;
 

commit 723330f023d1b5e9ed4a266854d7f43c156f78fb

Fix ICCAVR build that had broken by r4724. As stated already: Such header dependencies are a big mess.
git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@4729 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

nut/pro/uhttp/utils.c
@@ -37,11 +37,11 @@
  */
 
 #include <pro/uhttp/utils.h>
+#include <memdebug.h>
 
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
-#include <memdebug.h>
 
 void HttpUrlUnescape(char *str)
 {