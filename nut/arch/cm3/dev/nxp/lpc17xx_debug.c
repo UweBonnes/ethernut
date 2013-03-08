9ffd682b511c7b5e31d455921a9f57f4ce1e5653
olereinhardt <olereinhardt@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2013-03-08 10:34:25 +0000

    Fixed USART / Debug USART baudrate calculation for LPC176x CPUs and take peripheral clock prescaler into account
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@5010 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- nut/arch/cm3/dev/nxp/lpc17xx_debug.c
+++ nut/arch/cm3/dev/nxp/lpc17xx_debug.c
@@ -153,6 +153,23 @@ static int Lpc17xxDevDebugSetSpeed(uint32_t baudrate)
     /* get UART block clock */
     uart_clock = NutArchClockGet(NUT_HWCLK_PCLK);
 
+#if defined(MCU_LPC176x)
+
+    if ((LPC_UART_TypeDef*)USARTn == LPC_UART0) {
+        uart_clock /= Lpc176x_PclkDivGet(CLKPWR_PCLKSEL_UART0);
+    } else
+    if ((LPC_UART1_TypeDef*)USARTn == LPC_UART1) {
+        uart_clock /= Lpc176x_PclkDivGet(CLKPWR_PCLKSEL_UART1);
+    } else
+    if ((LPC_UART_TypeDef*)USARTn == LPC_UART2) {
+        uart_clock /= Lpc176x_PclkDivGet(CLKPWR_PCLKSEL_UART2);
+    } else
+    if ((LPC_UART_TypeDef*)USARTn == LPC_UART3) {
+        uart_clock /= Lpc176x_PclkDivGet(CLKPWR_PCLKSEL_UART3);
+    }
+
+#endif
+
     uart_clock = uart_clock >> 4; /* div by 16 */
 
     /* Baudrate calculation is done according the following formula:
@@ -673,7 +690,7 @@ static int Lpc17xxDevDebugInit(NUTDEVICE * dev)
 
     Lpc17xxDevDebugSetSpeed(DEBUG_INIT_BAUTRATE);
     Lpc17xxDevDebugSetDataBits(8);
-    Lpc17xxDevDebugSetStopBits(2);
+    Lpc17xxDevDebugSetStopBits(1);
     Lpc17xxDevDebugSetParity(0);
 
     return 0;
