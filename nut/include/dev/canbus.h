UweBonnes/ethernut

commit cb44aed1ac4b16aae9d0ffe14f967887d73c7cdb

STM32F30: Adapt CAN
git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@5075 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

nut/include/dev/canbus.h
@@ -186,6 +186,10 @@ typedef struct _CANFILTER CANFILTER;
  * \brief _NUTCANBUS
  */
 
+#if defined(MCU_CM3)
+#include <arch/cm3/stm/stm32_can.h>
+#endif
+
 extern int NutRegisterCanBus( NUTCANBUS *bus, int entries );
 extern int CanAddFilter( NUTCANBUS *bus, CANFILTER *filter );
 extern int CanSetBaud( NUTCANBUS *bus, int baud, uint32_t alt_btr);

nut/arch/cm3/dev/stm/ih_stm32_can1.c
@@ -43,7 +43,7 @@
 #define NUT_IRQPRI_CAN1  4
 #endif
 
-#if defined(STM32F10X_LD)|| defined(STM32F10X_MD)|| defined(STM32F10X_HD)|| defined(STM32F10X_XL)
+#if defined(STM32F10X_LD)|| defined(STM32F10X_MD)|| defined(STM32F10X_HD)|| defined(STM32F10X_XL) || defined(STM32F30X)
 #define CAN_TX_IRQn  USB_HP_CAN1_TX_IRQn
 #define CAN_RX0_IRQn USB_LP_CAN1_RX0_IRQn
 #define CAN_RX1_IRQn CAN1_RX1_IRQn

nut/arch/cm3/dev/stm/stm32_can.c
@@ -1,7 +1,8 @@
 /*
  * Copyright (C) 2010 by Ulrich Prinz (uprinz2@netscape.net)
  * Copyright (C) 2010 by Rittal GmbH & Co. KG. All rights reserved.
- * Copyright (C) 2012 by Uwe Bonnes(bon@elektron.ikp.physik.tu-darmstadt.de)
+ * Copyright (C) 2012/2013 by Uwe Bonnes
+ *                          (bon@elektron.ikp.physik.tu-darmstadt.de)
  *
  *
  * Redistribution and use in source and binary forms, with or without
@@ -506,7 +507,7 @@ int CanGetFeatures( NUTCANBUS *bus, uint32_t flags)
 #define CAN_BS1(x) (((x-1) & 0xf) << 16)
 #define CAN_BS2(x) (((x-1) & 0x7) << 20)
 #define CAN_SJW(x) (((x-1) & 0x3) << 24)
-#if defined(MCU_STM32F1)
+#if defined(MCU_STM32F1) || defined(STM32F30X)
 #define STM_CAN_ABP1_CHECK 36000000
 #define STM_CAN_BTR_1M   (CAN_APB1_DIV(2)  |CAN_BS1(15)|CAN_BS2(2)|CAN_SJW(2))
 #define STM_CAN_BTR_800k (CAN_APB1_DIV(3)  |CAN_BS1(12)|CAN_BS2(2)|CAN_SJW(2))
@@ -599,6 +600,7 @@ int CanSetBaud( NUTCANBUS *bus, int baud, uint32_t alt_btr)
        F1: 36 MHz
        L1: 32 MHz
        F2: 30 MHz
+       F3: 30 MHz
        F4: 42 MHz
        Assume this frequency for defining the BTR register for now.
        If we need to cope with other APB1 Clocks, this gets much more

nut/arch/cm3/dev/stm/stm32_can1.c
@@ -88,7 +88,7 @@
   #define CAN1RX_GPIO_PIN    11
   #define CAN1TX_GPIO_PIN    12
  #endif
-#else /*L1/F2/F4*/
+#else /*L1/F2/F30/F4*/
  #if !defined(CAN1_TX_PIN)
   #if (CANBUS1_REMAP_CAN == 0)
    #define CAN1TX_GPIO_PORT  NUTGPIO_PORTA
@@ -141,6 +141,10 @@
  #endif
 #endif
 
+#if defined(STM32F30X)
+#define F3_GPIO_AF_CAN1(x) (x == NUTGPIO_PORTD)?7:9
+#endif
+
 /*!
  * \brief Processor specific Hardware Initiliaization
  *
@@ -174,6 +178,9 @@ int Stm32CanHw1Init(void)
 #if defined (MCU_STM32F1)
     AFIO->MAPR &= ~AFIO_MAPR_CAN_REMAP;
     AFIO->MAPR |=  CANBUS_REMAP;
+#elif defined (MCU_STM32F30)
+    GPIO_PinAFConfig((GPIO_TypeDef*) CAN1RX_GPIO_PORT, CAN1RX_GPIO_PIN, F3_GPIO_AF_CAN1(CAN1RX_GPIO_PORT));
+    GPIO_PinAFConfig((GPIO_TypeDef*) CAN1TX_GPIO_PORT, CAN1TX_GPIO_PIN, F3_GPIO_AF_CAN1(CAN1RX_GPIO_PORT));
 #else
     GPIO_PinAFConfig((GPIO_TypeDef*) CAN1RX_GPIO_PORT, CAN1RX_GPIO_PIN, GPIO_AF_CAN1);
     GPIO_PinAFConfig((GPIO_TypeDef*) CAN1TX_GPIO_PORT, CAN1TX_GPIO_PIN, GPIO_AF_CAN1);

nut/conf/arch/cm3/stm32f30.nut
@@ -82,6 +82,7 @@ nutarch_cm3_stm32f30 =
                       "HW_UART3_STM32",
                       "HW_UART4_STM32",
                       "HW_UART5_STM32",
+                      "HW_CAN1_STM32",
                 },
                 file = "include/cfg/arch.h"
             }

nut/conf/f3_discovery.conf
@@ -11,3 +11,4 @@ DEBUG_USE_UART = "DEV_UART1"
 USART1_TX_PIN  = "4"
 USART1_RX_PIN  = "5"
 CRT_UNSETENV_POSIX = ""
+CANBUS1_REMAP_CAN = "1"

nut/include/arch/cm3/board/f3_discovery.h
@@ -40,19 +40,19 @@
  * $Id$
  * \endverbatim
  */
-/* LD3 North RED*/
+/* LD4 NW BLUE*/
 #ifndef HEARTBEAT_IDLE_PORT
 #define HEARTBEAT_IDLE_PORT NUTGPIO_PORTE
 #endif
 #ifndef HEARTBEAT_IDLE_PIN
-#define HEARTBEAT_IDLE_PIN 9
+#define HEARTBEAT_IDLE_PIN 12
 #endif
-/* LD4 NW BLUE*/
+/* LD3 North RED*/
 #ifndef LED1_PORT
 #define LED1_PORT NUTGPIO_PORTE
 #endif
 #ifndef LED1_PIN
-#define LED1_PIN 12
+#define LED1_PIN 9
 #endif
 /* LD6 WEST BLUE*/
 #ifndef LED1_PORT
@@ -62,7 +62,6 @@
 #define LED2_PIN 15
 #endif
 
-#include <dev/null.h>
 #ifndef DEV_UART
 #define DEV_UART devUsartStm32_1
 #endif
@@ -70,4 +69,10 @@
 #define DEV_UART_NAME  devUsartStm32_1.dev_name
 #endif
 
-#undef DEF_TWIBUS
+#ifndef DEV_CANBUS
+#define DEV_CANBUS Stm32CanBus1
+#endif
+
+#ifndef DEV_CANBUS_SLAVE
+#define  DEV_CANBUS_SLAVE Stm32CanBus1C
+#endif

commit ca97b246877dc597a7339f2aa1a69de98b7b05c4

Fix CM3 CAN bus compile, which broke in r5075 for all non-STM32 Cortex targets.
git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@5092 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

nut/include/dev/canbus.h
@@ -186,10 +186,6 @@ typedef struct _CANFILTER CANFILTER;
  * \brief _NUTCANBUS
  */
 
-#if defined(MCU_CM3)
-#include <arch/cm3/stm/stm32_can.h>
-#endif
-
 extern int NutRegisterCanBus( NUTCANBUS *bus, int entries );
 extern int CanAddFilter( NUTCANBUS *bus, CANFILTER *filter );
 extern int CanSetBaud( NUTCANBUS *bus, int baud, uint32_t alt_btr);