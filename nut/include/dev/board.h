358bd509d97d80af28531fd07216955c908b6f53
u_bonnes <u_bonnes@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2013-06-10 16:52:02 +0000

    STM32F4: Add a configuration for the F4 Discovery with baseboard STM32F4_BB.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@5191 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- nut/include/dev/board.h
+++ nut/include/dev/board.h
@@ -158,6 +158,8 @@
 #include <arch/avr32/board/evk1105.h>
 #elif defined(F1_DISCOVERY)
 #include <arch/cm3/board/f1_discovery.h>
+#elif defined(STM32F4_BB)
+#include <arch/cm3/board/stm32f4_bb.h>
 #elif defined(F4_DISCOVERY)
 #include <arch/cm3/board/f4_discovery.h>
 #elif defined(F3_DISCOVERY)
