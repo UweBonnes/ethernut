e3d5264dfda74aeddb0aef90b2f54c1a1cb26423
u_bonnes <u_bonnes@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2014-01-07 18:16:56 +0000

    include/cfg/owi.h: Allow compilation without OWI configured.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@5524 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

---
+++ nut/include/cfg/owi.h
@@ -0,0 +1,18 @@
+#ifndef _CFG_OWI_H_
+#define _CFG_OWI_H_
+
+/*!
+ * \file cfg/owi.h
+ * \brief OWI configuration.
+ *
+ * When using the Configurator, a file with the same name may be created in the
+ * build tree, which replaces this file.
+ *
+ * \verbatim
+ *
+ * $Id$
+ *
+ * \endverbatim
+ */
+
+#endif
