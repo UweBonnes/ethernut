ef77ff7134edcb1726ebe6e125f7fd5bda75909d
u_bonnes <u_bonnes@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2014-05-07 16:22:15 +0000

    gpio_stm32.h: Add missing braces.
    if (GpioPinGet(bank, bit) == 0) on STM32F3 was missinterpreted!
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@5692 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- nut/include/dev/gpio_stm32.h
+++ nut/include/dev/gpio_stm32.h
@@ -179,7 +179,7 @@ extern int GpioPortConfigSet(int bank, uint32_t mask, uint32_t flags);
 
 #if defined(MCU_STM32F0) ||defined(MCU_STM32F3)
 /* GPIO on AHB2 is outside of bitband region */
-#define GpioPinGet(bank, bit)        (CM3REG((bank), GPIO_TypeDef, IDR ) & (1<<(bit)))?1:0
+#define GpioPinGet(bank, bit)        ((CM3REG((bank), GPIO_TypeDef, IDR ) & (1<<(bit)))?1:0)
 #define GpioPinSet(bank, bit, value) (((volatile uint16_t*)((bank) + offsetof(GPIO_TypeDef, BSRR)))[(value)?0:1] = (1<<(bit)))
 #define GpioPinSetHigh(bank, bit)    (CM3REG((bank), GPIO_TypeDef, BSRR) = (1<<(bit)))
 #define GpioPinSetLow(bank, bit)     (((volatile uint16_t*)((bank) + offsetof(GPIO_TypeDef, BSRR)))[1] = (1<<(bit)))
