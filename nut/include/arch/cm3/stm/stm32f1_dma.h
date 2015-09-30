ed0d345dfba28d04050c40a4afcf93034aa19267
u_bonnes <u_bonnes@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2015-09-30 14:44:55 +0000

    stm32f1_dma.h: Remove definitions needed during stdlib -> cube transition.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@6162 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- nut/include/arch/cm3/stm/stm32f1_dma.h
+++ nut/include/arch/cm3/stm/stm32f1_dma.h
@@ -209,63 +209,6 @@
 #define ADC2_ALT_DMA_IRQ               sig_DMA1_CH4
 #endif
 
-/* RM0008 defines the bits for CCR1, CCR2 etc. only.
- * Equalize the names by using the general names from STM32F3*/
-#if !defined(DMA_CCR_EN) && defined(DMA_CCR1_EN)
-#define DMA_CCR_EN DMA_CCR1_EN
-#endif
-#if !defined(DMA_CCR_TCIE) && defined(DMA_CCR1_TCIE)
-#define DMA_CCR_TCIE DMA_CCR1_TCIE
-#endif
-#if !defined(DMA_CCR_HTIE) && defined(DMA_CCR1_HTIE)
-#define DMA_CCR_HTIE DMA_CCR1_HTIE
-#endif
-#if !defined(DMA_CCR_TEIE) && defined(DMA_CCR1_TEIE)
-#define DMA_CCR_TEIE DMA_CCR1_TEIE
-#endif
-#if !defined(DMA_CCR_DIR) && defined(DMA_CCR1_DIR)
-#define DMA_CCR_DIR DMA_CCR1_DIR
-#endif
-#if !defined(DMA_CCR_CIRC) && defined(DMA_CCR1_CIRC)
-#define DMA_CCR_CIRC DMA_CCR1_CIRC
-#endif
-#if !defined(DMA_CCR_PINC) && defined(DMA_CCR1_PINC)
-#define DMA_CCR_PINC DMA_CCR1_PINC
-#endif
-#if !defined(DMA_CCR_MINC) && defined(DMA_CCR1_MINC)
-#define DMA_CCR_MINC DMA_CCR1_MINC
-#endif
-#if !defined(DMA_CCR_PSIZE) && defined(DMA_CCR1_PSIZE)
-#define DMA_CCR_PSIZE DMA_CCR1_PSIZE
-#endif
-#if !defined(DMA_CCR_PSIZE_0) && defined(DMA_CCR1_PSIZE_0)
-#define DMA_CCR_PSIZE_0 DMA_CCR1_PSIZE_0
-#endif
-#if !defined(DMA_CCR_PSIZE_1) && defined(DMA_CCR1_PSIZE_1)
-#define DMA_CCR_PSIZE_1 DMA_CCR1_PSIZE_1
-#endif
-#if !defined(DMA_CCR_MSIZE) && defined(DMA_CCR1_MSIZE)
-#define DMA_CCR_MSIZE DMA_CCR1_MSIZE
-#endif
-#if !defined(DMA_CCR_MSIZE_0) && defined(DMA_CCR1_MSIZE_0)
-#define DMA_CCR_MSIZE_0 DMA_CCR1_MSIZE_0
-#endif
-#if !defined(DMA_CCR_MSIZE_1) && defined(DMA_CCR1_MSIZE_1)
-#define DMA_CCR_MSIZE_1 DMA_CCR1_MSIZE_1
-#endif
-#if !defined(DMA_CCR_PL) && defined(DMA_CCR1_PL)
-#define DMA_CCR_PL DMA_CCR1_PL
-#endif
-#if !defined(DMA_CCR_PL_0) && defined(DMA_CCR1_PL_0)
-#define DMA_CCR_PL_0 DMA_CCR1_PL_0
-#endif
-#if !defined(DMA_CCR_PL_1) && defined(DMA_CCR1_PL_1)
-#define DMA_CCR_PL_1 DMA_CCR1_PL_1
-#endif
-#if !defined(DMA_CCR_MEM2MEM) && defined(DMA_CCR1_MEM2MEM)
-#define DMA_CCR_MEM2MEM DMA_CCR1_MEM2MEM
-#endif
-
 /*!
  * \brief STM32 F0/F1/F3/L0/L3 DMA Control Flags.
  */
