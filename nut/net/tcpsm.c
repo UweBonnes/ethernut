UweBonnes/ethernut

commit 8189c10b4af5914c0469dffa3a164f1817cad97d

This should fix the problem of incoming TCP packets eating up all memory. First, the buffer of packets received in advance is limited. Second, small segments will be collected into one.
git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@2771 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

nut/net/tcpsm.c
@@ -284,10 +284,14 @@ NETBUF *volatile tcp_in_nbq;
 static uint16_t tcp_in_cnt;
 static HANDLE tcpThread = 0;
 
+static size_t tcp_adv_cnt;
+static size_t tcp_adv_max = TCP_WINSIZE;
+
 /* ================================================================
  * Helper functions
  * ================================================================
  */
+
 
 /*!
  * \brief Reads TCP option fields if any, and writes the data to
@@ -380,6 +384,22 @@ static void NutTcpProcessAppData(TCPSOCKET * sock, NETBUF * nb)
     else
         sock->so_tx_flags |= SO_FORCE;
 
+    if (++sock->so_rx_apc > 8) {
+        NETBUF *nbq;
+        int_fast8_t apc = sock->so_rx_apc;
+        int cnt = sock->so_rx_cnt;
+
+        for (nbq = sock->so_rx_buf; nbq; nbq = nbq->nb_next) {
+            if (nbq->nb_ap.sz < 256) {
+                sock->so_rx_apc -= NutNetBufCollect(nbq, cnt);
+                break;
+            }
+            if (--apc < 8) {
+                break;
+            }
+            cnt -= nbq->nb_ap.sz;
+        }
+    }
     NutTcpOutput(sock, 0, 0);
 }
 
@@ -1193,29 +1213,38 @@ static void NutTcpStateEstablished(TCPSOCKET * sock, uint8_t flags, TCPHDR * th,
         uint32_t thq_seq;
 
         if (nb->nb_ap.sz) {
-            nbq = sock->so_rx_nbq;
-            nbqp = &sock->so_rx_nbq;
-            while (nbq) {
-                thq = (TCPHDR *) (nbq->nb_tp.vp);
-                th_seq = htonl(th->th_seq);
-                thq_seq = htonl(thq->th_seq);
-                if (th_seq < thq_seq) {
+            /* Keep track of the number of bytes used by packets
+            ** received in advance. Honor a global limit. */
+            tcp_adv_cnt += nb->nb_dl.sz + sizeof(IPHDR) + sizeof (TCPHDR) + nb->nb_ap.sz;
+            if (tcp_adv_cnt > tcp_adv_max) {
+                /* Limit reached, discard the packet. */
+                NutNetBufFree(nb);
+                tcp_adv_cnt -= nb->nb_dl.sz + sizeof(IPHDR) + sizeof (TCPHDR) + nb->nb_ap.sz;
+            } else {
+                nbq = sock->so_rx_nbq;
+                nbqp = &sock->so_rx_nbq;
+                while (nbq) {
+                    thq = (TCPHDR *) (nbq->nb_tp.vp);
+                    th_seq = htonl(th->th_seq);
+                    thq_seq = htonl(thq->th_seq);
+                    if (th_seq < thq_seq) {
+                        *nbqp = nb;
+                        nb->nb_next = nbq;
+                        break;
+                    }
+                    if (th_seq == thq_seq) {
+                        NutNetBufFree(nb);
+                        sock->so_tx_flags |= SO_ACK | SO_FORCE;
+                        NutTcpOutput(sock, 0, 0);
+                        return;
+                    }
+                    nbqp = &nbq->nb_next;
+                    nbq = nbq->nb_next;
+                }
+                if (nbq == 0) {
                     *nbqp = nb;
-                    nb->nb_next = nbq;
-                    break;
+                    nb->nb_next = 0;
                 }
-                if (th_seq == thq_seq) {
-                    NutNetBufFree(nb);
-                    sock->so_tx_flags |= SO_ACK | SO_FORCE;
-                    NutTcpOutput(sock, 0, 0);
-                    return;
-                }
-                nbqp = &nbq->nb_next;
-                nbq = nbq->nb_next;
-            }
-            if (nbq == 0) {
-                *nbqp = nb;
-                nb->nb_next = 0;
             }
         } else
             NutNetBufFree(nb);
@@ -1253,6 +1282,9 @@ static void NutTcpStateEstablished(TCPSOCKET * sock, uint8_t flags, TCPHDR * th,
             if (htonl(th->th_seq) > sock->so_rx_nxt)
                 break;
             sock->so_rx_nbq = nb->nb_next;
+            /* Update the heap space used by packets 
+            ** received in advance. */
+            tcp_adv_cnt -= nb->nb_dl.sz + sizeof(IPHDR) + sizeof (TCPHDR) + nb->nb_ap.sz;
             if (htonl(th->th_seq) == sock->so_rx_nxt) {
                 NutTcpProcessAppData(sock, nb);
                 flags |= th->th_flags;

nut/dev/netbuf.c
@@ -315,4 +315,44 @@ void NutNetBufFree(NETBUF * nb)
     NutHeapFree(nb);
 }
 
+/*!
+ * \brief Collect linked list of network buffers.
+ *
+ * \param nbq Points to an existing network buffer queue.
+ *
+ * \return Number of released network buffers.
+ */
+int NutNetBufCollect(NETBUF * nbq, int total)
+{
+    int rc = 0;
+    NETBUF *nb;
+    NETBUF *nbx;
+    NBDATA nbd;
+    uint8_t *ap;
+
+    if (NutNetBufAllocData(&nbd, total) == 0) {
+        ap = (uint8_t *) nbd.vp;
+        memcpy(nbd.vp, nbq->nb_ap.vp, nbq->nb_ap.sz);
+        nbd.sz = nbq->nb_ap.sz;
+        nb = nbq->nb_next;
+        while (nb && total >= nbd.sz + nb->nb_ap.sz) {
+            nbx = nb->nb_next;
+            memcpy(ap + nbd.sz, nb->nb_ap.vp, nb->nb_ap.sz);
+            nbd.sz += nb->nb_ap.sz;
+            NutNetBufFree(nb);
+            nb = nbx;
+            rc++;
+        }
+        nbq->nb_next = nb;
+        if (nbq->nb_flags & NBAF_APPLICATION) {
+            NutHeapFree(nbq->nb_ap.vp);
+        } else {
+            nbq->nb_flags |= NBAF_APPLICATION;
+        }
+        nbq->nb_ap.vp = nbd.vp;
+        nbq->nb_ap.sz = nbd.sz;
+    }
+    return rc;
+}
+
 /*@}*/

nut/include/dev/netbuf.h
@@ -171,6 +171,7 @@ __BEGIN_DECLS
 extern NETBUF *NutNetBufAlloc(NETBUF *nb, uint8_t type, int size);
 extern NETBUF *NutNetBufClone(NETBUF *nb);
 extern void NutNetBufFree(NETBUF *nb);
+extern int NutNetBufCollect(NETBUF * nbq, int total);
 
 __END_DECLS
 

nut/include/sys/sock_var.h
@@ -244,6 +244,7 @@ struct tcp_socket {
     int     so_rx_cnt;      /*!< \brief Number of data bytes in the receive buffer. */
     int     so_rx_bsz;      /*!< \brief Receive buffer size. */
     int     so_rd_cnt;      /*!< \brief Number of bytes read from buffer top. */
+    int_fast8_t so_rx_apc;  /*!< \brief Number of packets received in advance. */
     NETBUF  *so_rx_buf;     /*!< \brief Data waiting to be read by application. */
     HANDLE  so_rx_tq;       /*!< \brief Threads waiting for received data. */
     NETBUF  *so_rx_nbq;     /*!< \brief Network buffers received in advance. */

nut/net/tcpsock.c
@@ -816,6 +816,7 @@ int NutTcpReceive(TCPSOCKET * sock, void *data, int size)
                 sock->so_rx_buf = nb->nb_next;
                 sock->so_rx_cnt -= rd_cnt;
                 NutNetBufFree(nb);
+                sock->so_rx_apc--;
                 nb = sock->so_rx_buf;
                 rd_cnt = 0;
             }

commit d84764f5f37006132dd73a29ccce21ba96fc36d9

Do not access just freed memory when keeping track of packetes received in advance
This bug was introduced in r2771 and might result in invalid values for 
tcp_adv_cnt, which will result in dropping packets without any need, resulting
in connection hangup etc.



git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@6284 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

nut/net/tcpsm.c
@@ -1238,8 +1238,8 @@ static void NutTcpStateEstablished(TCPSOCKET * sock, uint8_t flags, TCPHDR * th,
             tcp_adv_cnt += nb->nb_dl.sz + sizeof(IPHDR) + sizeof(TCPHDR) + nb->nb_ap.sz;
             if (tcp_adv_cnt > tcp_adv_max) {
                 /* Limit reached, discard the packet. */
-                NutNetBufFree(nb);
                 tcp_adv_cnt -= nb->nb_dl.sz + sizeof(IPHDR) + sizeof(TCPHDR) + nb->nb_ap.sz;
+                NutNetBufFree(nb);
             } else {
                 nbq = sock->so_rx_nbq;
                 nbqp = &sock->so_rx_nbq;