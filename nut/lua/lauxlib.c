ea50be5805cd3b92336b067b0d1bfe9d06541487
u_bonnes <u_bonnes@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2017-01-10 10:04:51 +0000

    Fix warnings with gcc-6 caused by strange indent.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@6578 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- nut/lua/lauxlib.c
+++ nut/lua/lauxlib.c
@@ -605,11 +605,11 @@ LUALIB_API int luaL_loadfile (lua_State *L, const char *filename) {
     if (c == '\n') c = getc(lf.f);
   }
   if (c == LUA_SIGNATURE[0] && filename) {  /* binary file? */
-    lf.f = freopen(filename, "rb", lf.f);  /* reopen in binary mode */
-    if (lf.f == NULL) return errfile(L, "reopen", fnameindex);
-    /* skip eventual `#!...' */
-   while ((c = getc(lf.f)) != EOF && c != LUA_SIGNATURE[0]) ;
-    lf.extraline = 0;
+      lf.f = freopen(filename, "rb", lf.f);  /* reopen in binary mode */
+      if (lf.f == NULL) return errfile(L, "reopen", fnameindex);
+      /* skip eventual `#!...' */
+      while ((c = getc(lf.f)) != EOF && c != LUA_SIGNATURE[0]) ;
+      lf.extraline = 0;
   }
   ungetc(c, lf.f);
   status = lua_load(L, getF, &lf, lua_tostring(L, -1));
