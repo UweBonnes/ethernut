7be5ce2dafbbd725c64a71192287c18a6b923e95
u_bonnes <u_bonnes@1ca430b3-8271-4a6b-9b0e-3dcac98a8924>
2020-02-12 13:56:03 +0000

    board/f4_discovery.h: Use I2C1 as default i2cbus.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ethernut/code/trunk@6882 1ca430b3-8271-4a6b-9b0e-3dcac98a8924

--- nut/include/arch/cm3/board/f4_discovery.h
+++ nut/include/arch/cm3/board/f4_discovery.h
@@ -127,13 +127,8 @@
 #endif
 
 #ifndef DEV_I2CBUS
-#if 0
 #define DEV_I2CBUS i2cBus1Stm32
 #include <dev/i2cbus_stm32.h>
-#else
-#define DEV_I2CBUS i2cBus0Gpio
-#include <dev/i2cbus_gpio.h>
-#endif
 #endif
 
 #include <cfg/devices.h>
